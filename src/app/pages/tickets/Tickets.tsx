943f4c7f0482bdb49ef3a86723bbb490b1246ff5
sfwnisme <safymo424@gmail.com>
2024-11-19 04:53:38 +0300

    refactor: create grid for the tickets page

--- src/app/pages/tickets/Tickets.tsx
+++ src/app/pages/tickets/Tickets.tsx
@@ -1,31 +1,19 @@
-import React from 'react'
-import List from '../../../components/list/List'
-import ListItem from '../../../components/list/ListItem'
-import Button from '../../../components/button/Button'
-import AvatarDropdown from '../../../components/avatar-dropdown/AvatarDropdown'
-import Avatar from '../../../components/avatar/Avatar'
-import HelpText from "../../../components/help-text/HelpText.tsx";
-
+import Ticket from '../../../components/ticket/Ticket.tsx'
+import S from './Tickets.module.css'
 type Props = {}
 
 export default function Tickets({ }: Props) {
   return (
     <div>
       Tickets Page
-<HelpText></HelpText>
-      <AvatarDropdown>
-        <List position='absolute' rightOrLeft='left'>
-          <ListItem>home</ListItem>
-          <ListItem>profile</ListItem>
-          <ListItem href='23'>
-            settings
-          </ListItem>
-          <ListItem noStyle>
-            <Button size='xs' variant='info' width='fill'>Login</Button>
-            <Button size='xs' variant='success' width='fill'>Register</Button>
-          </ListItem>
-        </List>
-      </AvatarDropdown>
+      <div className={S.tickets}>
+        <Ticket type='complaint' />
+        <Ticket type='suggestion' />
+        <Ticket type='inquiry' />
+        <Ticket />
+        <Ticket />
+        <Ticket />
+      </div>
     </div >
   )
 }
