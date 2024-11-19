5adcd7de9d70c92f6dfda4b46ce6e1e3795097ea
sfwnisme <safymo424@gmail.com>
2024-11-19 05:53:16 +0300

    refactor: remove unused snippets and linse, and changed the props names

--- src/app/pages/tickets/id/components/TicketHeader.tsx
+++ src/app/pages/tickets/id/components/TicketHeader.tsx
@@ -1,41 +1,41 @@
 import S from '../Ticket.module.css'
 import Badge from '../../../../../components/badge/Badge'
-import {Timer, User} from 'lucide-react'
+import { Timer, User } from 'lucide-react'
 import Button from '../../../../../components/button/Button'
 
 type Props = {
-  toggle: boolean;
-  setToggle: { (value: boolean | ((prev: boolean) => boolean)): void }
+  isSidebarOpen: boolean;
+  setIsSidebarOpen: { (value: boolean | ((prev: boolean) => boolean)): void }
 }
 
 export default function TicketHeader(props: Props) {
- 
+
   return (
     <header className={S['ticket-page__header']}>
       <div className={S['ticket-page__info']}>
         <h3 className={S['ticket-page__title']}>Ticket Title</h3>
         <div className={S['ticket-page__details']}>
-          <span className={S['ticket-page__detail']}><User size={14}/>sfwn</span>
-          <span className={S['ticket-page__detail']}><Timer size={14}/>2 weeks ago</span>
+          <span className={S['ticket-page__detail']}><User size={14} />sfwn</span>
+          <span className={S['ticket-page__detail']}><Timer size={14} />2 weeks ago</span>
           <span className={S['ticket-page__detail']}>
-            <Badge text='payment' variant='primary'/>
-            <Badge text='visa' variant='primary'/>
-            <Badge text='coupon' variant='primary'/>
+            <Badge text='payment' variant='primary' />
+            <Badge text='visa' variant='primary' />
+            <Badge text='coupon' variant='primary' />
           </span>
         </div>
       </div>
       <Button
-        variant={props.toggle ? 'info' : 'danger'}
+        variant={props.isSidebarOpen ? 'info' : 'danger'}
         // outline
         size='square'
         width='fit'
-        className={`${S['ticket-page__collapse-btn']} ${!props.toggle && S['ticket-page__collapse-btn--fixed']}`}
-        onClick={() => props.setToggle((prev: boolean) => !prev)}
+        className={`${S['ticket-page__collapse-btn']} ${props.isSidebarOpen && S['ticket-page__collapse-btn--fixed']}`}
+        onClick={() => props.setIsSidebarOpen((prev: boolean) => !prev)}
       >
         {
-          props.toggle
+          props.isSidebarOpen
             ? 'more'
-            : 'collapse'
+            : 'close'
         }
         {/* more */}
       </Button>
