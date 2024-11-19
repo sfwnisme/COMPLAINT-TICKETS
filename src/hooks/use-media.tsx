e929a40cb4b0fd1f1a569b69ca92243afa8e307b
sfwnisme <safymo424@gmail.com>
2024-11-19 05:41:34 +0300

    feat: create use media hook to tigger the page width size

---
+++ src/hooks/use-media.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+
+export default function useMedia() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    console.log(screenWidth)
+    window.addEventListener('resize', () => setScreenWidth(window.innerWidth))
+    return () => window.removeEventListener('resize', () => setScreenWidth(window.innerWidth))
+  })
+
+  return screenWidth
+}
