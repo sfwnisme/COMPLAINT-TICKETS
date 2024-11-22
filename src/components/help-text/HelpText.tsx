137b8a558ccf0e59ef9efe65ab3557e058d56187
sfwnisme <safymo424@gmail.com>
2024-11-22 05:36:54 +0300

    refactor: added variants

--- src/components/help-text/HelpText.tsx
+++ src/components/help-text/HelpText.tsx
@@ -1,13 +1,24 @@
 import { Info } from "lucide-react";
 import S from './HelpText.module.css'
+import { TVariants } from "../defintions.components";
 
 type Props = {
   children: React.ReactNode,
+  variant?: TVariants,
   icon?: 'visible' | 'invisible'
 }
 
-export default function HelpText({ children = 'help text for short information', icon = 'visible' }: Props) {
+export default function HelpText({ children = 'help text for short information', variant = 'primary', icon = 'visible' }: Props) {
+
+  const variants = {
+    primary: S['help-text--primary'],
+    info: S['help-text--info'],
+    success: S['help-text--success'],
+    warning: S['help-text--warning'],
+    danger: S['help-text--danger'],
+  }
+
   return (
-    <p className={S['help-text']}>{icon === 'visible' && <Info size={13} className={S['help-text__icon']} />} {children}</p>
+    <p className={`${S['help-text']} ${variants[variant]}`}>{icon === 'visible' && <Info size={13} className={S['help-text__icon']} />} {children}</p>
   )
 }
