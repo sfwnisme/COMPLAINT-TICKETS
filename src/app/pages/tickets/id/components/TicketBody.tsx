52c427e7d6c1454c1bafc23eac65ec599d9ede30
sfwnisme <safymo424@gmail.com>
2024-11-22 05:33:32 +0300

    refactor: added the marked as a solution ui for the comments

--- src/app/pages/tickets/id/components/TicketBody.tsx
+++ src/app/pages/tickets/id/components/TicketBody.tsx
@@ -4,6 +4,8 @@ import Avatar from '../../../../../components/avatar/Avatar'
 import AvatarDropdown from '../../../../../components/avatar-dropdown/AvatarDropdown'
 import List from '../../../../../components/list/List'
 import ListItem from '../../../../../components/list/ListItem'
+import Badge from '../../../../../components/badge/Badge'
+import HelpText from '../../../../../components/help-text/HelpText'
 
 // type Props = {}
 
@@ -22,130 +24,125 @@ export default function TicketBody() {
       </div>
       <div className={`${S["ticket-page__replies"]}`}>
         <div className={`${S["ticket-page__reply"]}`}>
-          <div className={`${S['ticket-page__relpy']}`}>
-            <div className={`${S['ticket-page__replyer']}`}>
-              <AvatarDropdown>
-                <List position='absolute' rightOrLeft='left'>
-                  <ListItem>profile</ListItem>
-                  <ListItem>tickets</ListItem>
-                </List>
-              </AvatarDropdown>
-              <div>
-                <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
-                <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
-              </div>
+          <div className={`${S['ticket-page__replyer']}`}>
+            <AvatarDropdown>
+              <List position='absolute' rightOrLeft='left'>
+                <ListItem>profile</ListItem>
+                <ListItem>tickets</ListItem>
+                <ListItem>mark as a solution</ListItem>
+              </List>
+            </AvatarDropdown>
+            <div>
+              <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
+              <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
             </div>
-            <p className={`${S['ticket-page__replyer__message']}`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
-              reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
-              explicabo ipsam ex officiis maxime repudiandae nisi.
-            </p>
           </div>
+          <p className={`${S['ticket-page__replyer__message']}`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
+            reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
+            explicabo ipsam ex officiis maxime repudiandae nisi.
+          </p>
         </div>
         <div className={`${S["ticket-page__reply"]}`}>
-          <div className={`${S['ticket-page__relpy']}`}>
-            <div className={`${S['ticket-page__replyer']}`}>
-              <AvatarDropdown>
-                <List position='absolute' rightOrLeft='left'>
-                  <ListItem>profile</ListItem>
-                  <ListItem>tickets</ListItem>
-                </List>
-              </AvatarDropdown>
-              <div>
-                <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
-                <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
-              </div>
+          <div className={`${S['ticket-page__replyer']}`}>
+            <AvatarDropdown>
+              <List position='absolute' rightOrLeft='left'>
+                <ListItem>profile</ListItem>
+                <ListItem>tickets</ListItem>
+                <ListItem>mark as a solution</ListItem>
+              </List>
+            </AvatarDropdown>
+            <div>
+              <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
+              <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
             </div>
-            <p className={`${S['ticket-page__replyer__message']}`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
-              reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
-              explicabo ipsam ex officiis maxime repudiandae nisi.
-            </p>
           </div>
+          <p className={`${S['ticket-page__replyer__message']}`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
+            reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
+            explicabo ipsam ex officiis maxime repudiandae nisi.
+          </p>
         </div>
         <div className={`${S["ticket-page__reply"]}`}>
-          <div className={`${S['ticket-page__relpy']}`}>
-            <div className={`${S['ticket-page__replyer']}`}>
-              <AvatarDropdown>
-                <List position='absolute' rightOrLeft='left'>
-                  <ListItem>profile</ListItem>
-                  <ListItem>tickets</ListItem>
-                </List>
-              </AvatarDropdown>
-              <div>
-                <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
-                <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
-              </div>
+          <div className={`${S['ticket-page__replyer']}`}>
+            <AvatarDropdown>
+              <List position='absolute' rightOrLeft='left'>
+                <ListItem>profile</ListItem>
+                <ListItem>tickets</ListItem>
+                <ListItem>mark as a solution</ListItem>
+              </List>
+            </AvatarDropdown>
+            <div>
+              <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
+              <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
             </div>
-            <p className={`${S['ticket-page__replyer__message']}`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
-              reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
-              explicabo ipsam ex officiis maxime repudiandae nisi.
-            </p>
           </div>
+          <p className={`${S['ticket-page__replyer__message']}`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
+            reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
+            explicabo ipsam ex officiis maxime repudiandae nisi.
+          </p>
+        </div>
+        <div className={`${S["ticket-page__reply"]} ${S['ticket-page__reply-marked-as-a-solution']}`}>
+          <div className={`${S['ticket-page__replyer']}`}>
+            <AvatarDropdown>
+              <List position='absolute' rightOrLeft='left'>
+                <ListItem>profile</ListItem>
+                <ListItem>tickets</ListItem>
+                <ListItem>mark as a solution</ListItem>
+              </List>
+            </AvatarDropdown>
+            <div>
+              <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
+              <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
+            </div>
+          </div>
+          <p className={`${S['ticket-page__replyer__message']}`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
+            reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
+            explicabo ipsam ex officiis maxime repudiandae nisi.
+          </p>
+          <HelpText variant='success'>this response marked as a solution</HelpText>
         </div>
         <div className={`${S["ticket-page__reply"]}`}>
-          <div className={`${S['ticket-page__relpy']}`}>
-            <div className={`${S['ticket-page__replyer']}`}>
-              <AvatarDropdown>
-                <List position='absolute' rightOrLeft='left'>
-                  <ListItem>profile</ListItem>
-                  <ListItem>tickets</ListItem>
-                </List>
-              </AvatarDropdown>
-              <div>
-                <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
-                <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
-              </div>
+          <div className={`${S['ticket-page__replyer']}`}>
+            <AvatarDropdown>
+              <List position='absolute' rightOrLeft='left'>
+                <ListItem>profile</ListItem>
+                <ListItem>tickets</ListItem>
+                <ListItem>mark as a solution</ListItem>
+              </List>
+            </AvatarDropdown>
+            <div>
+              <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
+              <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
             </div>
-            <p className={`${S['ticket-page__replyer__message']}`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
-              reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
-              explicabo ipsam ex officiis maxime repudiandae nisi.
-            </p>
           </div>
+          <p className={`${S['ticket-page__replyer__message']}`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
+            reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
+            explicabo ipsam ex officiis maxime repudiandae nisi.
+          </p>
         </div>
         <div className={`${S["ticket-page__reply"]}`}>
-          <div className={`${S['ticket-page__relpy']}`}>
-            <div className={`${S['ticket-page__replyer']}`}>
-              <AvatarDropdown>
-                <List position='absolute' rightOrLeft='left'>
-                  <ListItem>profile</ListItem>
-                  <ListItem>tickets</ListItem>
-                </List>
-              </AvatarDropdown>
-              <div>
-                <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
-                <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
-              </div>
+          <div className={`${S['ticket-page__replyer']}`}>
+            <AvatarDropdown>
+              <List position='absolute' rightOrLeft='left'>
+                <ListItem>profile</ListItem>
+                <ListItem>tickets</ListItem>
+                <ListItem>mark as a solution</ListItem>
+              </List>
+            </AvatarDropdown>
+            <div>
+              <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
+              <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
             </div>
-            <p className={`${S['ticket-page__replyer__message']}`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
-              reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
-              explicabo ipsam ex officiis maxime repudiandae nisi.
-            </p>
           </div>
-        </div>
-        <div className={`${S["ticket-page__reply"]}`}>
-          <div className={`${S['ticket-page__relpy']}`}>
-            <div className={`${S['ticket-page__replyer']}`}>
-              <AvatarDropdown>
-                <List position='absolute' rightOrLeft='left'>
-                  <ListItem>profile</ListItem>
-                  <ListItem>tickets</ListItem>
-                </List>
-              </AvatarDropdown>
-              <div>
-                <p className={`${S['ticket-page__replyer__name']}`}>Ali Khaled</p>
-                <p className={`${S['ticket-page__replyer__date']}`}>Mar 26, 2024 03:34 PM (3 weeks ago)</p>
-              </div>
-            </div>
-            <p className={`${S['ticket-page__replyer__message']}`}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
-              reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
-              explicabo ipsam ex officiis maxime repudiandae nisi.
-            </p>
-          </div>
+          <p className={`${S['ticket-page__replyer__message']}`}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis porro consectetur
+            reprehenderit illum voluptatem nam quod quis, necessitatibus nihil soluta eius numquam ea
+            explicabo ipsam ex officiis maxime repudiandae nisi.
+          </p>
         </div>
 
       </div>
