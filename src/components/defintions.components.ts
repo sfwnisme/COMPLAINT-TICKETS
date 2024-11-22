21533b63fcec190d6180113c6b6ba0633d3686fc
sfwnisme <safymo424@gmail.com>
2024-11-22 05:34:09 +0300

    refactor: new types

--- src/components/defintions.components.ts
+++ src/components/defintions.components.ts
@@ -1,2 +1,3 @@
 export type TSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type TVariants = 'primary' | 'info' | 'warning' | 'success' | 'danger';
+export type TTicketTypes = 'suggestion' | 'complaint' | 'inquiry'
