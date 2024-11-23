5c705d8fb6e21ee2453bd5e9c15a991af133580e
sfwnisme <safymo424@gmail.com>
2024-11-23 04:27:32 +0300

    feat(Dropdown.tsx)+delete: created a new dropdown component + deleted the previous one

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,43 +1,24 @@
-import { ArrowDown, ChevronDown, ChevronUp } from 'lucide-react'
+import React, { useState } from 'react'
 import S from './Dropdown.module.css'
-import { useCallback, useState } from 'react'
-import Button from '../button/Button'
+import { EllipsisVertical } from 'lucide-react';
+import Button from '../button/Button';
 
-type Props = {}
+type Props = {
+  children: React.ReactNode;
+  // position: 'right' | 'left'
+} & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
-let id = 0
-
-const dummyListData = [
-  { id: id++, title: 'About' },
-  { id: id++, title: 'Dashboard' },
-  { id: id++, title: 'Profile' },
-  { id: id++, title: 'Settings' },
-]
-
-export default function Dropdown({ list = dummyListData }: Props) {
-  const [isOpenDropdown, setIsOpenDropdown] = useState(false)
-
-  const handleOpenDropdown = useCallback(() => {
-    setIsOpenDropdown((prev) => !prev)
-  }, [])
-
-  const listContent = list.map((l: { id: number, title: string }) =>
-    <li className={S.dropdown_list_item} id={l.id.toString()} key={l.id}>{l.title}</li>
-  )
-
+export default function Dropdown({ children }: Props) {
+  const [toggle, setToggle] = useState(false)
+  const handleListToggle = () => {
+    setToggle((prev) => !prev)
+  }
   return (
-    <div className={`${S.dropdown}`}>
-      <Button outline size='sm' onClick={handleOpenDropdown}>
-        <span className={S.dropdown_title}>
-          Options
-          <ChevronUp size={18} className={`${isOpenDropdown ? S.dropdown_title_arrow_up : S.dropdown_title_arrow_down}`} />
-        </span></Button>
-      {
-        isOpenDropdown &&
-        <ul className={S.dropdown_list} onClick={handleOpenDropdown}>
-          {listContent}
-        </ul>
-      }
+    <div className={S['dropdown']} onClick={handleListToggle}>
+      <Button outline size='square'>
+        <EllipsisVertical size={15} className={S['dropdown__icon']} />
+      </Button>
+      {toggle && children}
     </div>
   )
 }
