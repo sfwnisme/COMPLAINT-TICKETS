422d3b96024c6c4a3679044887149f7fc4edaafd
sfwnisme <safymo424@gmail.com>
2025-04-09 02:55:15 +0300

    feat: close dropdown on click outside list, and yaxix prop options

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import S from './Dropdown.module.css'
 import { EllipsisVertical } from 'lucide-react';
 import Button from '../button/Button';
@@ -10,11 +10,31 @@ type Props = {
 
 export default function Dropdown({ children }: Props) {
   const [toggle, setToggle] = useState(false)
+  const dropdownRef = useRef(null)
   const handleListToggle = () => {
     setToggle((prev) => !prev)
   }
+  console.log('global', toggle)
+
+  useEffect(() => {
+    console.log('effect', toggle)
+    const handleClickedOutside = (event) => {
+      console.log('invoked', dropdownRef.current)
+      if (dropdownRef.current && !dropdownRef.current?.contains(event.target)) {
+        // if (dropdownRef.current && !event.target) {
+        setToggle(false)
+      }
+    }
+
+    if (toggle) {
+      document.addEventListener('click', handleClickedOutside)
+      return () => {
+        document.removeEventListener('click', handleClickedOutside)
+      }
+    }
+  })
   return (
-    <div className={S['dropdown']} onClick={handleListToggle}>
+    <div className={S['dropdown']} onClick={handleListToggle} ref={dropdownRef}>
       <Button outline size='square'>
         <EllipsisVertical size={15} className={S['dropdown__icon']} />
       </Button>
--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -3,13 +3,14 @@ import S from './List.module.css'
 type Props = {
   children: React.ReactNode,
   position: 'absolute' | 'static',
-  rightOrLeft: 'right' | 'left'
+  rightOrLeft: 'right' | 'left',
+  yaxis?: 'top' | 'bottom'
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>
-export default function List({ children, position = 'static', rightOrLeft = 'left', ...rest }: Props) {
+export default function List({ children, position = 'static', rightOrLeft = 'left', yaxis = 'bottom', ...rest }: Props) {
   console.log(rightOrLeft, position)
   return (
     <ul
-      className={`${S.list} ${position === 'absolute' && S['list--absolute']} ${rightOrLeft === 'right' ? S['list--right'] : S['list--left']}`}
+      className={`${S.list} ${position === 'absolute' && S['list--absolute']} ${rightOrLeft === 'right' ? S['list--right'] : S['list--left']} ${yaxis === 'bottom' ? S['list--absolute--bottom'] : S['list--absolute--top']}`}
       {...rest}
     >{children}</ul>
   )
