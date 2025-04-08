8e05b71e956b942ec100755e2f501a48d1815e9b
sfwnisme <safymo424@gmail.com>
2025-04-09 02:56:17 +0300

    refactor: implement success, warning, and error message

--- src/hooks/use-api-message.tsx
+++ src/hooks/use-api-message.tsx
@@ -3,7 +3,9 @@ import HelpText from '../components/help-text/HelpText'
 import { TVariants } from '../components/defintions.components'
 
 
-export default function useApiMessage(msg: string | { [key: string]: string }[] | null | undefined = '', type: TVariants): React.ReactElement {
+export default function useApiMessage(msg: string | { [key: string]: string }[] | null | undefined = '', status: number): React.ReactElement {
+
+  const statusVariant = status >= 400 ? 'danger' : status >= 300 && status <= 399 ? 'warning' : 'success'
 
   // let renderMessage: React.ReactNode | string
   if (!msg) {
@@ -15,7 +17,7 @@ export default function useApiMessage(msg: string | { [key: string]: string }[] 
       <div style={{ display: 'flex', flexDirection: 'column', gap: '2px' }}>
         {
           msg.map((m, idx) => (
-            <HelpText variant={type} key={m.path} icon='invisible'>
+            <HelpText variant={statusVariant} key={m.path} icon='invisible'>
               {idx + 1} - <strong>{m.path}:</strong> {m.msg}
             </HelpText>
           ))
@@ -25,8 +27,8 @@ export default function useApiMessage(msg: string | { [key: string]: string }[] 
   }
 
   return (
-    <div>
+    <HelpText variant={statusVariant} icon='invisible'>
       {msg}
-    </div>
+    </HelpText>
   )
 }
