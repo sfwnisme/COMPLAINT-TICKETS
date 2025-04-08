ce9857321392063e506ac2298406524c48f74517
sfwnisme <safymo424@gmail.com>
2025-04-09 02:58:40 +0300

    refactor: implement the UI and changed the dashboard layout folder name

--- src/components/forms/create-user-form/CreateUserForm.tsx
+++ src/components/forms/create-user-form/CreateUserForm.tsx
@@ -2,14 +2,15 @@ import S from "./CreateUserForm.module.css";
 import Input from "../../input/Input.tsx";
 import Select from "../../select/Select.tsx";
 import { USER_ROLES } from "../../../constrains/constrains.tsx";
-import Title from "../../title/Title.tsx";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Button from "../../button/Button.tsx";
 import Loader from "../../loaders/loader/Loader.tsx";
 import useCreateApiData from "../../../hooks/use-create-api-data.tsx";
 import useApiMessage from "../../../hooks/use-api-message.tsx";
-import { AxiosError } from "axios";
 import { TAxiosError } from "../../../types/types.ts";
+import Spacer from "../../spacer/Spacer.tsx";
+import Logo from '../../../assets/logo.png'
+import PasswordInput from "../../input/PasswordInput.tsx";
 
 type Inputs = {
   name: string;
@@ -22,26 +23,36 @@ export default function CreateUserForm() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const { mutateAsync: createUser, isPending, error } = useCreateApiData<Inputs>('/users/register', 'post')
+  const { mutateAsync: createUser, isPending, isSuccess, isError, error } = useCreateApiData<Inputs>('/users/register', 'post')
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     try {
-      // console.log("new user data", data);
       const res = await createUser(data);
       return res;
     } catch (error) {
       console.error("create user error", error);
     }
   };
-  const errorMessage = useApiMessage((error as TAxiosError)?.response?.data?.msg, "danger")
+  let responseMessage
+  if (isError) {
+    responseMessage = useApiMessage((error as TAxiosError)?.response?.data?.msg, error?.status)
+  } else if (isSuccess) {
+    responseMessage = useApiMessage('user created successfully', 201)
+  }
+  // const errorMessage = useApiMessage((error as TAxiosError)?.response?.data?.msg, error?.status)
   console.log(errors)
 
   return (
     <div className={S.create_user_form__container}>
-      {errorMessage}
-      <Title text="Create User" />
+      <div className={S['form-header']}>
+        {/* <div>
+          <img src={Logo} height={40} className={S['form-header-logo']} alt='logo' />
+        </div> */}
+        <h2 className={S['form-header-title']}>Create New User</h2>
+        <p className={S['form-header-subtitle']}>please enter your details to sign in</p>
+      </div>
+      <Spacer size='lg' />
       <form className={S.create_user_form} onSubmit={handleSubmit(onSubmit)}>
         <Input
           type="text"
@@ -55,7 +66,7 @@ export default function CreateUserForm() {
           placeholder="email"
           title="Email"
         />
-        <Input
+        <PasswordInput
           type="password"
           placeholder="password"
           title="Password"
@@ -71,7 +82,10 @@ export default function CreateUserForm() {
             </option>
           ))}
         </Select>
-        <Button>{isPending ? <Loader /> : "Create"}</Button>
+        <Button width="fill">{isPending ? <Loader /> : "Create"}</Button>
+        <Spacer />
+        {/* {errorMessage} */}
+        {responseMessage}
       </form>
     </div>
   );
