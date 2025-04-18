6fdd8f99d22ba4abd3dbcb3ebb03395481862797
sfwnisme <safymo424@gmail.com>
2025-04-18 16:56:16 +0300

    refactor(UpdateUser.tsx): satup the component

--- src/app/pages/users/update/UpdateUser.tsx
+++ src/app/pages/users/update/UpdateUser.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import { useParams } from 'react-router-dom'
-
+import UpdateUserForm from '../../../../components/forms/update-user-form/UpdateUserForm'
 
 export default function UpdateUser() {
   const { userId } = useParams()
   console.log(userId)
   return (
-    <div>UpdateUser: {userId}</div>
+    <div>
+      <UpdateUserForm />
+    </div>
   )
 }
