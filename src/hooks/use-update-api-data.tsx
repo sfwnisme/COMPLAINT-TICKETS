54f1f9f77e26bc50ea9b3b6822f9373b9873f884
sfwnisme <safymo424@gmail.com>
2025-04-19 02:26:34 +0300

    refactor: clean up the component

--- src/hooks/use-update-api-data.tsx
+++ src/hooks/use-update-api-data.tsx
@@ -2,25 +2,21 @@ import { useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-export default function useUpdateApiData<T /** the data types */>(endpoint: string, method: 'post' | 'patch' = 'post') {
+export default function useUpdateApiData<T /** the data types */>(endpoint: string, method: 'put' | 'patch' = 'patch') {
   const mutation = useMutation({
     mutationKey: ['update-api-data', endpoint],
     mutationFn: async (data: T) => {
       axios.defaults.headers.common['Authorization'] = `Bearer ${Cookies.get('TOKEN')}`
       const BASE_URL = import.meta.env.VITE_BASE_URL + endpoint
-      if (method === 'post') {
-        const res = await axios.patch(BASE_URL, data)
-        return res
-      }
       if (method === 'patch') {
         const res = await axios.patch(BASE_URL, data)
         return res
       }
+      if (method === 'put') {
+        const res = await axios.put(BASE_URL, data)
+        return res
+      }
     },
-    onSuccess: (res) => {
-      console.log('onSuccess update api data', res)
-      return res
-    }
   })
 
   return mutation
