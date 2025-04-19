5b4265ae1ef071039e1f4c6df9c492ba4471bc3b
sfwnisme <safymo424@gmail.com>
2025-04-19 04:12:32 +0300

    refactor: format the component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -21,13 +21,14 @@ const Input = React.forwardRef<HTMLInputElement, finalType>(({ variant = 'primar
   }
 
   const settings = `${S.input} ${variants[variant]}`
-  return (<div className={S['input-wrapper']}>
-    {title && <label htmlFor={rest.name} className={S.label}>
-      {title || 'Input ttitle'}
-    </label>}
-    <input className={settings} id={rest.name} placeholder="fill the input" ref={ref} {...rest} />
-    {message && <HelpText icon='invisible' variant={variant}>{message}</HelpText>}
-  </div>
+  return (
+    <div className={S['input-wrapper']}>
+      {title && <label htmlFor={rest.name} className={S.label}>
+        {title || 'Input ttitle'}
+      </label>}
+      <input className={settings} id={rest.name} placeholder="fill the input" ref={ref} {...rest} />
+      {message && <HelpText icon='invisible' variant={variant}>{message}</HelpText>}
+    </div>
   )
 })
 
