952b55d7287491c1c45266a0691fd11236ed6fef
sfwnisme <safymo424@gmail.com>
2025-04-19 04:11:37 +0300

    feat: implement update user validation with zod

--- src/components/forms/update-user-form/UpdateUserForm.tsx
+++ src/components/forms/update-user-form/UpdateUserForm.tsx
@@ -1,7 +1,7 @@
 import S from "./UpdateUserForm.module.css";
 import Input from "../../input/Input.tsx";
 import Select from "../../select/Select.tsx";
-import { USER_ROLES } from "../../../constrains/constrains.tsx";
+import { USER_ROLES, USER_ROLES_VALUES } from "../../../constrains/constrains.tsx";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Button from "../../button/Button.tsx";
 import Loader from "../../loaders/loader/Loader.tsx";
@@ -12,13 +12,24 @@ import useUpdateApiData from "../../../hooks/use-update-api-data.tsx";
 import { useParams } from "react-router-dom";
 import useGetSingleApiData from "../../../hooks/use-get-single-api-data.tsx";
 import axios from "axios";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type Inputs = {
-  name: string;
-  email: string;
-  password?: string;
-  role: string;
-};
+// type Inputs = {
+//   name: string;
+//   email: string;
+//   password?: string;
+//   role: string;
+// };
+
+const userSchema = z.object({
+  name: z.string().min(8, { message: 'name must be at least 8 characters' }),
+  email: z.string().email(),
+  password: z.string().optional(),
+  role: z.enum(USER_ROLES_VALUES)
+})
+
+type Inputs = z.infer<typeof userSchema>
 
 export default function UpdateUserForm() {
   const { userId } = useParams()
@@ -28,17 +39,19 @@ export default function UpdateUserForm() {
   const {
     register,
     handleSubmit,
-    // formState,
+    formState: { errors },
   } = useForm<Inputs>({
-    reValidateMode: 'onChange',
+    resolver: zodResolver(userSchema),
+    mode: 'all',
+    // reValidateMode: 'onChange',
     values: {
       name: data?.name,
       email: data?.email,
       role: data?.role
     }
   });
-  const { mutateAsync: updateUser, isPending, isSuccess, error } = useUpdateApiData<Inputs>(`/users/${userId}`, 'post')
-  console.log('//////////////////',error)
+  const { mutateAsync: updateUser, isPending, isSuccess, error } = useUpdateApiData<Inputs>(`/users/${userId}`, 'patch')
+  console.log('//////////////////', error)
 
   let typedError
   if (axios.isAxiosError(error)) {
@@ -69,18 +82,24 @@ export default function UpdateUserForm() {
           {...register("name")}
           placeholder="name"
           title="Name"
+          message={errors?.name?.message}
+          variant={errors?.name && 'danger'}
         />
         <Input
           type="text"
           {...register("email")}
           placeholder="email"
           title="Email"
+          message={errors?.email?.message}
+          variant={errors?.email && 'danger'}
         />
         <PasswordInput
           type="password"
           placeholder="password"
           title="Password"
           {...register("password")}
+          message={errors?.password?.message}
+          variant={errors?.password && 'danger'}
         />
         <Select {...register("role")} title="Role" id="roles">
           <option disabled value="">
