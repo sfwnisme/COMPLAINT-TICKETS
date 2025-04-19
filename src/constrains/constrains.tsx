1059a674474ea12e88add2a11e19abf10c8f2f7c
sfwnisme <safymo424@gmail.com>
2025-04-19 04:13:42 +0300

    feat: create user values array

--- src/constrains/constrains.tsx
+++ src/constrains/constrains.tsx
@@ -1,13 +1,16 @@
-export const USER_ROLES = {
+import { Role } from "../types/types"
+
+export const USER_ROLES: { [key: string]: Role } = {
   ADMIN: 'admin',
   MANAGER: 'manager',
   CSR: 'csr',
   VIEW_ONLY: 'view_only',
 }
+export const USER_ROLES_VALUES = ['admin', 'manager', 'csr', 'view_only'] as const
 
 export const USER_ROLES_COLORS = {
   admin: 'success',
-  manager: 'warngin',
+  manager: 'warning',
   csr: 'info',
   view_only: 'primary'
 }
