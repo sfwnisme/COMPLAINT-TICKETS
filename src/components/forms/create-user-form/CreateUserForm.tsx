1477f3fc95e03f7cb80ee88d7f031f9b23f61e4c
sfwnisme <safymo424@gmail.com>
2025-04-21 18:29:09 +0300

    feat(CreateUserForm.tsx): implement the data validation using zod and guard the error of the query

--- src/components/forms/create-user-form/CreateUserForm.tsx
+++ src/components/forms/create-user-form/CreateUserForm.tsx
@@ -1,7 +1,7 @@
 import S from "./CreateUserForm.module.css";
 import Input from "../../input/Input.tsx";
 import Select from "../../select/Select.tsx";
-import { USER_ROLES, USER_ROLES_VALUES } from "../../../constrains/constrains.tsx";
+import { USER_ROLES } from "../../../constrains/constrains.tsx";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Button from "../../button/Button.tsx";
 import Loader from "../../loaders/loader/Loader.tsx";
@@ -12,26 +12,21 @@ import PasswordInput from "../../input/PasswordInput.tsx";
 import axios from "axios";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { createUserSchema } from "../../../validation/user.validation.tsx";
 
-const userSchema = z.object({
-  name: z.string().min(8, { message: '8 character at least' }),
-  email: z.string().email(),
-  password: z.string().min(8, {message: 'password at least should have 8 characters'}),
-  role: z.enum(USER_ROLES_VALUES)
-})
-type Inputs = z.infer<typeof userSchema>
-
+type Inputs = z.infer<typeof createUserSchema>;
 export default function CreateUserForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>({
-    resolver: zodResolver(userSchema),
-    mode: "all"
+    resolver: zodResolver(createUserSchema),
+    mode: "all",
   });
-  const { mutateAsync, isPending, isSuccess, error } = useCreateApiData<Inputs>('/users/register')
-
+  const { mutateAsync, isPending, isSuccess, error } =
+    useCreateApiData<Inputs>("/users/register");
+  console.log("error for create user", error);
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     try {
       const res = await mutateAsync(data);
@@ -41,21 +36,26 @@ export default function CreateUserForm() {
     }
   };
 
-  let typedError
+  let typedError;
   if (axios.isAxiosError(error)) {
-    typedError = error
+    typedError = error;
   }
 
-  const errorMessage = useApiMessage(typedError?.response?.data?.msg, typedError?.status ?? 400)
-  const successMessage = useApiMessage('user created successfully', 201)
+  const errorMessage = useApiMessage(
+    typedError?.response?.data?.msg,
+    typedError?.status ?? 400,
+  );
+  const successMessage = useApiMessage("user created successfully", 201);
 
   return (
     <div className={S.create_user_form__container}>
-      <div className={S['form-header']}>
-        <h2 className={S['form-header-title']}>Create New User</h2>
-        <p className={S['form-header-subtitle']}>please enter your details to sign in</p>
+      <div className={S["form-header"]}>
+        <h2 className={S["form-header-title"]}>Create New User</h2>
+        <p className={S["form-header-subtitle"]}>
+          please enter your details to sign in
+        </p>
       </div>
-      <Spacer size='lg' />
+      <Spacer size="lg" />
       <form className={S.create_user_form} onSubmit={handleSubmit(onSubmit)}>
         <Input
           type="text"
@@ -63,7 +63,7 @@ export default function CreateUserForm() {
           placeholder="name"
           title="Name"
           message={errors?.name?.message}
-          variant={errors?.name && 'danger'}
+          variant={errors?.name && "danger"}
         />
         <Input
           type="text"
@@ -71,7 +71,7 @@ export default function CreateUserForm() {
           placeholder="email"
           title="Email"
           message={errors?.email?.message}
-          variant={errors?.email && 'danger'}
+          variant={errors?.email && "danger"}
         />
         <PasswordInput
           type="password"
@@ -79,7 +79,7 @@ export default function CreateUserForm() {
           title="Password"
           {...register("password")}
           message={errors?.password?.message}
-          variant={errors?.password && 'danger'}
+          variant={errors?.password && "danger"}
         />
         <Select {...register("role")} title="Role" id="roles">
           <option disabled value="">
