e2092438e15bc10b40b9604b806dbe566010eff4
sfwnisme <safymo424@gmail.com>
2025-04-22 01:26:45 +0300

    feat(Alert.tsx): implement icon based on variants, and show or hide icons using single prop

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,15 +1,16 @@
 import S from './Alert.module.css'
 import React from 'react'
 import { TVariants } from '../defintions.components'
+import { CircleAlert, CircleCheck, CircleHelp, Info, TriangleAlert } from 'lucide-react'
 
 type Props = {
   children: React.ReactNode,
   variant?: TVariants,
-  icon?: React.ReactNode,
+  hasIcon?: boolean,
   visible: boolean
 }
 
-export default function Alert({ children, variant = 'primary', icon, visible = false }: Readonly<Props>) {
+export default function Alert({ children, variant = 'primary', hasIcon, visible = false }: Readonly<Props>) {
   const variants = {
     primary: S['alert--primary'],
     info: S['alert--info'],
@@ -17,13 +18,20 @@ export default function Alert({ children, variant = 'primary', icon, visible = f
     warning: S['alert--warning'],
     danger: S['alert--danger'],
   }
+  const icons = {
+    primary: <CircleHelp />,
+    info: <Info />,
+    success: <CircleCheck />,
+    warning: <TriangleAlert />,
+    danger: <CircleAlert />,
+  }
   return (
     <>
       {visible && <div className={`${S.alert} ${variants[variant]}`}>
         {
-          icon &&
+          hasIcon &&
           <div className={S['alert__icon']}>
-            {icon}
+            {icons[variant]}
           </div>
         }
         <p className={S['alert_content']}>
