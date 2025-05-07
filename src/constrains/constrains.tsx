c3dcc8aed0aa3a0bae42994e9e949c7cb3268d13
sfwnisme <safymo424@gmail.com>
2025-05-07 13:19:49 +0300

    feat(constrains.ts): create ticket priorities' and statuses' colors and variants

--- src/constrains/constrains.tsx
+++ src/constrains/constrains.tsx
@@ -1,5 +1,7 @@
+import { TVariants } from "../components/defintions.components"
 import { Role } from "../types/types"
 
+// USER----------------------
 export const USER_ROLES: { [key: string]: Role } = {
   ADMIN: 'admin',
   MANAGER: 'manager',
@@ -7,10 +9,27 @@ export const USER_ROLES: { [key: string]: Role } = {
   VIEW_ONLY: 'view_only',
 }
 export const USER_ROLES_VALUES = ['admin', 'manager', 'csr', 'view_only'] as const
-
 export const USER_ROLES_COLORS = {
   admin: 'success',
   manager: 'warning',
   csr: 'info',
   view_only: 'primary'
 }
+// END USER----------------------
+
+// TICKET----------------------
+export const TICKET_STATUS = ['open', 'in-progress', 'resolved', 'closed'] as const
+export const TICKET_STATUS_COLORS: Record<string, TVariants> = {
+  open: 'primary',
+  'in-progress': 'info',
+  resolved: 'success',
+  closed: 'danger'
+} as const
+export const TICKET_PRIORITY = ['low', 'medium', 'high', 'critical'] as const
+export const TICKET_PRIORITY_COLORS: Record<string, TVariants> = {
+  low: 'primary',
+  medium: 'info',
+  high: 'warning',
+  critical: 'danger'
+} as const
+// END TICKET----------------------
