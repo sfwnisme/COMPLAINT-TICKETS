ecd82eeeadf3768ce520c69c64ab059a65fe4793
sfwnisme <safymo424@gmail.com>
2025-05-07 13:14:06 +0300

    refactor(definitions.components.ts): remove TTicketTypes type

--- src/components/defintions.components.ts
+++ src/components/defintions.components.ts
@@ -1,3 +1,2 @@
 export type TSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type TVariants = 'primary' | 'info' | 'warning' | 'success' | 'danger';
-export type TTicketTypes = 'suggestion' | 'complaint' | 'inquiry'
