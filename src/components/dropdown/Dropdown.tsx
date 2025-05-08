db39bfd1d34348df7e6b97c3efb5dce7ee308c1e
sfwnisme <safymo424@gmail.com>
2025-05-08 16:02:21 +0300

    fix(Dropdown.tsx): remove close list logic

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import S from './Dropdown.module.css'
 import { EllipsisVertical } from 'lucide-react';
 import Button from '../button/Button';
@@ -10,30 +10,12 @@ type Props = {
 
 export default function Dropdown({ children }: Props) {
   const [toggle, setToggle] = useState(false)
-  const dropdownRef = useRef(null)
   const handleListToggle = () => {
     setToggle((prev) => !prev)
   }
 
-  useEffect(() => {
-    console.log('effect', toggle)
-    const handleClickedOutside = (event) => {
-      console.log('invoked', dropdownRef.current)
-      if (dropdownRef.current && !dropdownRef.current?.contains(event.target)) {
-        // if (dropdownRef.current && !event.target) {
-        setToggle(false)
-      }
-    }
-
-    if (toggle) {
-      document.addEventListener('click', handleClickedOutside)
-      return () => {
-        document.removeEventListener('click', handleClickedOutside)
-      }
-    }
-  })
   return (
-    <div className={S['dropdown']} onClick={handleListToggle} ref={dropdownRef}>
+    <div className={S['dropdown']} onClick={handleListToggle}>
       <Button outline size='square'>
         <EllipsisVertical size={15} className={S['dropdown__icon']} />
       </Button>
