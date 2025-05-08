1d61e37f555ce1a9ff39e8a8d68ce79583b2650a
sfwnisme <safymo424@gmail.com>
2025-05-08 23:08:19 +0300

    feat: CreateCommentForm component

---
+++ src/features/forms/tickets/create-comment-form/CreateCommentForm.tsx
@@ -0,0 +1,49 @@
+import Style from './CreateCommentForm.module.css'
+import { SubmitHandler, useForm } from "react-hook-form"
+import useCreateApiData from "../../../../hooks/use-create-api-data"
+import Input from "../../../../components/input/Input"
+import Button from "../../../../components/button/Button"
+
+type Inputs = {
+  content: string,
+}
+
+export default function CreateCommentForm({ ticketId }: { ticketId: string }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    formState: { errors, isValid, isDirty, disabled },
+  } = useForm<Inputs>({
+    mode: 'all'
+  })
+  const { mutateAsync, isPending } = useCreateApiData<Inputs>({ endpoint: '/comments/create', revalidateKey: '/comments' })
+
+  const onSubmit: SubmitHandler<Inputs> = async (data, e) => {
+    const newComment = {
+      content: data?.content,
+      ticket: ticketId,
+      isSolution: false,
+    }
+    try {
+      const res = await mutateAsync(newComment)
+      console.log('success comment')
+      e?.target.reset()
+      return res
+    } catch (error) {
+      console.log('new comment error', error)
+    }
+  }
+  console.log(watch())
+  const disableButton = isPending || !isValid || getValues('content').length <= 5
+
+  return (
+    <form className={`${Style['ticket-page__add-comment']}`} onSubmit={handleSubmit(onSubmit)}>
+      <Input type='text' placeholder='Comment...'
+        {...register('content')}
+      />
+      <Button variant='info' size='lg' disabled={disableButton}>{isPending ? 'Sending...' : 'Send'}</Button>
+    </form>
+  )
+}
