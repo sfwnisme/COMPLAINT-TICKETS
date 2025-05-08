823fcbfe6be98462a386db8dbd6264c0ec5cc43c
sfwnisme <safymo424@gmail.com>
2025-05-09 00:51:38 +0300

    feat(use-update-api-data.tsx): change params to object param

--- src/hooks/use-update-api-data.tsx
+++ src/hooks/use-update-api-data.tsx
@@ -1,12 +1,25 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { axiosInstance } from '../libs/axios-instance'
 
-export default function useUpdateApiData<T /** the data types */>(endpoint: string, method: 'put' | 'patch' = 'patch') {
+// export default function useUpdateApiData<T /** the data types */>(endpoint: string, method: 'put' | 'patch' = 'patch') {
+type Props = {
+  endpoint: string,
+  revalidateKey: string,
+  id: string,
+  method: 'put' | 'patch'
+}
+export default function useUpdateApiData<T>({
+  endpoint = '',
+  revalidateKey = "",
+  id = "",
+  method = 'patch'
+}: Props) {
+  const queryClient = useQueryClient()
   const mutation = useMutation({
-    mutationKey: ['update-api-data', endpoint],
+    mutationKey: [`${endpoint}/${id}`],
     mutationFn: async (data: T) => {
       if (method === 'patch') {
-        const res = await axiosInstance.patch(endpoint, data)
+        const res = await axiosInstance.patch(`${endpoint}/${id}`, data)
         return res
       }
       if (method === 'put') {
@@ -14,6 +27,9 @@ export default function useUpdateApiData<T /** the data types */>(endpoint: stri
         return res
       }
     },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [revalidateKey] })
+    }
   })
 
   return mutation
