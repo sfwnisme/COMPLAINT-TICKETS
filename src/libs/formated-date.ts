18e0926ca0068a7be58a3ca1b4f2a6d97f0a2291
sfwnisme <safymo424@gmail.com>
2025-05-13 00:41:23 +0300

    feat(formated-date.ts): formate date function

---
+++ src/libs/formated-date.ts
@@ -0,0 +1,14 @@
+export const formatedDate = (date: Readonly<string> = "") => {
+  if (!date) return 'invalid date'
+  const isoDate = new Date(date)
+  const format = isoDate.toLocaleString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  })
+  return format
+}
