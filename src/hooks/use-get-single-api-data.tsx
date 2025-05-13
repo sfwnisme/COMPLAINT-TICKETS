587961c85ebb379e66d39f77f0438030f3651afd
sfwnisme <safymo424@gmail.com>
2025-05-13 21:19:39 +0300

    fix(use-get-single-api-data.tsx): apply generic types

--- src/hooks/use-get-single-api-data.tsx
+++ src/hooks/use-get-single-api-data.tsx
@@ -1,24 +1,33 @@
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance } from '../libs/axios-instance'
+import { AxiosResponse } from 'axios'
 
 type Params = {
   endpoint: string, id: string | undefined
 }
+// declare function 
+// useQuery
+// <TQueryFnData = unknown,
+//  TError = DefaultError,
+//  TData = TQueryFnData,
+//  TQueryKey extends QueryKey = QueryKey>
+// (options: UndefinedInitialDataOptions<TQueryFnData, TError, TData, TQueryKey>, queryClient?: QueryClient): UseQueryResult<TData, TError>;
 
-export default function useGetSingleApiData({ endpoint = '', id = '' }: Params) {
-  const query = useQuery({
-    queryKey: ['single-', endpoint],
-    queryFn: async () => {
-      const res = await axiosInstance.get(endpoint + '/' + id)
-      return res
-    },
-    select: (res) => {
-      console.log('onSuccess single api data', res)
+export default function useGetSingleApiData<T>({ endpoint = '', id = '' }: Params) {
+  const query = useQuery<T, Error>({
+    queryKey: ['single-', endpoint, id],
+    queryFn: async (): Promise<T> => {
+      const res: AxiosResponse<{ data: T }> = await axiosInstance.get(endpoint + '/' + id)
       return res.data.data
-    }
+    },
+    enabled: !!id,
+    // select: (res) => {
+    //   console.log('onSuccess single api data', res)
+    //   return res.data.data
+    // }
   })
 
-  console.log(query)
+  console.log('single query', query)
 
   return query
 }
