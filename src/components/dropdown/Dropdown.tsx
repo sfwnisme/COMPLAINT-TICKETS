e9df9b8686c6e5cf12f71c4989874c0a52a44fb7
sfwnisme <safymo424@gmail.com>
2025-05-16 04:03:37 +0300

    chore(dropdown): update button shape

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import S from './Dropdown.module.css'
 import { EllipsisVertical } from 'lucide-react';
 import Button from '../button/Button';
@@ -15,12 +15,12 @@ export default function Dropdown({ children }: Props) {
   }
 
   return (
-    <div className={S['dropdown']} onClick={handleListToggle}>
-      <Button outline size='square'>
+    <div className={S.dropdown}  onClick={handleListToggle} >
+      <Button shape='soft' size='square'>
         <EllipsisVertical size={15} className={S['dropdown__icon']} />
       </Button>
       {toggle && children}
-    </div>
+    </div >
   )
 }
 
