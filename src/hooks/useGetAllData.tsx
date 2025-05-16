92f509e75ae0b2d791ee2b3649dcf4b455e50285
sfwnisme <safymo424@gmail.com>
2025-05-16 04:25:05 +0300

    refactor(useGetAllData): remove react query's select function, and apply typescript generics

--- src/hooks/useGetAllData.tsx
+++ src/hooks/useGetAllData.tsx
@@ -1,16 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance } from '../libs/axios-instance'
+import { AxiosError } from 'axios'
 
-export default function useGetAllData(endpoint: string) {
-  const query = useQuery({
+export default function useGetAllData<T>(endpoint: string) {
+  const query = useQuery<T, AxiosError<{ msg?: string }>>({
     queryKey: [endpoint],
     queryFn: async () => {
       const res = await axiosInstance.get(endpoint)
-      return res
-    },
-    select: (response) => {
-      return response.data.data
-    },
+      return res.data.data
+    }
   })
   return query
 }
