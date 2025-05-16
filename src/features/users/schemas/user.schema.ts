1f9877c9c3ade49e57724e45b397eb4fa3903b2e
sfwnisme <safymo424@gmail.com>
2025-05-16 04:16:44 +0300

    feat(schema): user schema

---
+++ src/features/users/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { USER_ROLES_VALUES } from "../../../constraints/constraints";
+// import { USER_ROLES_VALUES } from "../constraints/constraints";
+
+const userSchema = {
+  name: z.string().min(3, 'name should be at least 3 characters').max(20, 'name should be maximum 20 characters '),
+  email: z.string().email(),
+  password: z.string().min(4, 'password should be at least 4 characters'),
+  role: z.enum(USER_ROLES_VALUES)
+}
+
+export const createUserSchema = z.object({
+  ...userSchema
+})
+
+export const updateUserSchema = z.object({
+  ...userSchema,
+  password: userSchema.password.optional()
+})
+
+export const loginSchema = z.object({
+  email: userSchema.email,
+  password: userSchema.password,
+})
