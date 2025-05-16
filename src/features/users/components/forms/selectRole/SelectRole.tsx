d8043acf17359b678d790cede72205d0d18457fc
sfwnisme <safymo424@gmail.com>
2025-05-16 04:29:10 +0300

    feat(selectRole): create dropdown select user role component

---
+++ src/features/users/components/forms/selectRole/SelectRole.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Select from '../../../../../components/select/Select'
+import { USER_ROLES } from '../../../../../constraints/constraints'
+type Props = {
+  title: string,
+  // id: string,
+} & React.DetailedHTMLProps<
+  React.SelectHTMLAttributes<HTMLSelectElement>,
+  HTMLSelectElement>
+const SelectRole = React.forwardRef<HTMLSelectElement, Props>(
+  ({ title, ...rest }, ref) => {
+    return (
+      <Select ref={ref} title={title} {...rest} >
+        <option disabled value="">
+          Select Role
+        </option>
+        {Object.values(USER_ROLES).map((role) => (
+          <option key={role} value={role} id={role}>
+            {role}
+          </option>
+        ))}
+      </Select>
+    )
+  }
+)
+export default SelectRole
+// export default function SelectRole({ title = 'Role', id = 'roles', register }: Props) {
+// // export default function SelectRole({ title = 'Role', id = 'roles', register }: Props) {
+//   return (
+//     <Select {...register} title={title} id={id}>
+//       <option disabled value="">
+//         Select Role
+//       </option>
+//       {Object.values(USER_ROLES).map((role) => (
+//         <option key={role} value={role} id={role}>
+//           {role}
+//         </option>
+//       ))}
+//     </Select>
+//   )
+// }
