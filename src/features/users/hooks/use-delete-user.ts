633c971184b43cc052c9459c0930bab7435b485b
sfwnisme <safymo424@gmail.com>
2025-05-17 14:39:13 +0300

    refactor(use-delete-user): add delete user logic in a hook

--- src/features/users/hooks/use-delete-user.ts
+++ src/features/users/hooks/use-delete-user.ts
@@ -1,5 +1,18 @@
+import { useCallback } from 'react'
 import useDeleteApiData from '../../../hooks/use-delete-api-data'
+import { useUsersStore } from '../../../store/users.store'
 
 export default function useDeleteUser() {
-  return useDeleteApiData({ endpoint: "/users", revalidateKey: "/users" })
+  const userId = useUsersStore((state) => state.userId)
+  const setUserId = useUsersStore((state) => state.setUserId)
+  const { mutateAsync, isPending } = useDeleteApiData({
+    endpoint: "/users", revalidateKey: "/users"
+  })
+
+  const onDeleteUser = useCallback(async () => {
+    await mutateAsync(userId)
+    setUserId('')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId])
+  return { onDeleteUser, isPending }
 }
