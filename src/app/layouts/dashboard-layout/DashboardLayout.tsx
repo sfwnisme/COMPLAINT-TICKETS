e0c82e280b6a5e52d50e06e58f153b182137d677
sfwnisme <safymo424@gmail.com>
2025-05-17 16:47:34 +0300

    style(DashboardLayout): change burger menu icon button shape

--- src/app/layouts/dashboard-layout/DashboardLayout.tsx
+++ src/app/layouts/dashboard-layout/DashboardLayout.tsx
@@ -1,35 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import S from './DashboardLayout.module.css'
 import Button from '../../../components/button/Button'
-import {Bell, Menu, Search, X} from 'lucide-react'
-import {dashboardSidebarLinks, sidebarSettingsLinks} from '../../../libs/data-center'
+import { Bell, Menu, Search, X } from 'lucide-react'
+import { dashboardSidebarLinks, sidebarSettingsLinks } from '../../../libs/data-center'
 import Input from '../../../components/input/Input'
 import useMedia from '../../../hooks/use-media'
 import Logo from '../../../assets/logo.png'
-import {NavLink, Outlet} from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import NavDropdown from '../../../components/nav-dropdown/NavDropdown'
 
-function Nav({collapseASide}: { readonly collapseASide: () => void }) {
+function Nav({ collapseASide }: { readonly collapseASide: () => void }) {
   return (
     <nav className={S.navbar}>
-      <Button onClick={collapseASide} size='square' outline>
-        <Menu size={24} strokeWidth={1.5}/>
+      <Button onClick={collapseASide} size='square' shape='soft'>
+        <Menu size={24} strokeWidth={1.5} />
       </Button>
       <span className={S['navbar__divider']}></span>
       <label className={S["navbar__search-box"]}>
-        <Search size={30} strokeWidth='1.5' className={S["navbar__search-icon"]}/>
-        <Input type='search' placeholder='Search...'/>
+        <Search size={30} strokeWidth='1.5' className={S["navbar__search-icon"]} />
+        <Input type='search' placeholder='Search...' />
       </label>
 
       <div className={`${S["navbar__notification"]} ${S['navbar__notification--active']}`}>
-        <Bell size={18} className={S['navbar__notification-icon']}/>
+        <Bell size={18} className={S['navbar__notification-icon']} />
       </div>
-      <NavDropdown/>
+      <NavDropdown />
     </nav>
   )
 }
 
-function ASide({collapseASide}: { readonly collapseASide: () => void }) {
+function ASide({ collapseASide }: { readonly collapseASide: () => void }) {
   const sidebarLinksContent = dashboardSidebarLinks?.map((link) => (
     <li key={link?.id} className={S['aside__item']}>
       <NavLink
@@ -37,7 +37,7 @@ function ASide({collapseASide}: { readonly collapseASide: () => void }) {
         title={link?.title}
         className={S.aside__link}
       >
-        <link.icon size={24} className={S['aside__icon']}/>
+        <link.icon size={24} className={S['aside__icon']} />
         {link?.title}
       </NavLink>
     </li>
@@ -50,7 +50,7 @@ function ASide({collapseASide}: { readonly collapseASide: () => void }) {
         title={link?.title}
         className={S.aside__link}
       >
-        <link.icon size={24} className={S['aside__icon']}/>
+        <link.icon size={24} className={S['aside__icon']} />
         {link?.title}
       </a>
     </li>
@@ -59,9 +59,9 @@ function ASide({collapseASide}: { readonly collapseASide: () => void }) {
   return (
     <aside className={S.aside}>
       <div className={S["aside__header"]}>
-        <img className={S['aside__logo']} src={Logo} alt=""/>
+        <img className={S['aside__logo']} src={Logo} alt="" />
         <Button size='square' variant='primary' className={S['aside__collapse-btn']} onClick={collapseASide}>
-          <X size={24}/>
+          <X size={24} />
         </Button>
       </div>
       <div className={S["aside__content"]}>
@@ -90,8 +90,8 @@ export default function DashboardLayout() {
   }
   return (
     <div className={`${S.layout} ${collapse && S.layout_collapse}`}>
-      <ASide collapseASide={collapseASide}/>
-      <Nav collapseASide={collapseASide}/>
+      <ASide collapseASide={collapseASide} />
+      <Nav collapseASide={collapseASide} />
       <main className={S.outlet}>
         {/*{children}*/}
         <Outlet />
