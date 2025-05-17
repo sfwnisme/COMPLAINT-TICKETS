293908eb83eb2fabb966233c7074e11cf6eaecbf
sfwnisme <safymo424@gmail.com>
2025-05-17 17:16:29 +0300

    feat(use-login): create custom hook to handle login logic

---
+++ src/features/users/hooks/use-login.ts
@@ -0,0 +1,43 @@
+import useCreateApiData from '../../../hooks/use-create-api-data'
+import { SubmitHandler } from 'react-hook-form'
+import { loginSchema } from '../schemas/user.schema'
+import Cookies from 'js-cookie'
+import { z } from 'zod'
+import axios from 'axios'
+
+type Inputs = z.infer<typeof loginSchema>
+
+export default function useLogin() {
+  const { mutateAsync, isPending, isError, isSuccess, error, data } = useCreateApiData({
+    endpoint: '/users/login',
+    revalidateKey: '/users/login',
+  })
+  Cookies.set('TOKEN', data?.data.data.token)
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const logedIn = await mutateAsync(data)
+      console.log('loged in successfully', logedIn)
+      window.location.pathname = '/dashboard'
+      return logedIn
+    } catch (error) {
+      console.log('login error', error)
+    }
+  }
+
+  let typedError;
+  if (axios.isAxiosError(error)) {
+    typedError = error
+  }
+  const errorMessage = typedError?.response?.data?.msg ?? typedError?.message
+  const successMessage = 'you logged in successfully'
+
+  return {
+    onSubmit,
+    isPending,
+    isError,
+    isSuccess,
+    successMessage,
+    errorMessage
+  }
+}
