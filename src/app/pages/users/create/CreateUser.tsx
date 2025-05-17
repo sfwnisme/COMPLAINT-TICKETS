bb840e6605e2eb8701e730f8671e621c6a481eef
sfwnisme <safymo424@gmail.com>
2025-05-17 15:20:33 +0300

    fix(users): remove user old components from pages dir

--- src/app/pages/users/create/CreateUser.tsx
+++
@@ -1,9 +0,0 @@
-// import S from './CreateUser.module.css'
-import CreateUserForm from "../../../../features/forms/users/create-user-form/CreateUserForm.tsx";
-export default function CreateUser() {
-  return (
-    <div>
-      <CreateUserForm />
-    </div>
-  )
-}
