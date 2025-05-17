05c6236475ed42c54e5b163c7da7aabff095458d
sfwnisme <safymo424@gmail.com>
2025-05-17 17:30:47 +0300

    feat(inputsConfigs): login inputs configs

--- src/features/users/inputsConfigs.ts
+++ src/features/users/inputsConfigs.ts
@@ -1,7 +1,8 @@
 import { FieldErrors, FieldValues, Path } from "react-hook-form";
 import { z } from "zod";
-import { createUserSchema, updateUserSchema } from "./schemas/user.schema";
+import { createUserSchema, loginSchema, updateUserSchema } from "./schemas/user.schema";
 
+type LoginFormInputs = z.infer<typeof loginSchema>
 type CreateUserFormInputs = z.infer<typeof createUserSchema>;
 type UpdateUserFormInputs = z.infer<typeof updateUserSchema>
 type InputsType<T extends FieldValues> = {
@@ -33,9 +34,18 @@ export const getupdateUserInputsConfigs = (errors: FieldErrors<UpdateUserFormInp
 export const getCreateUserInputsConfigs = (errors: FieldErrors<CreateUserFormInputs>) => {
   const inputs: InputsType<Omit<CreateUserFormInputs, 'role'>>[] = [
     { name: 'name', type: 'text', placeholder: 'Write name', title: 'Name', message: errors?.name && `${errors?.name?.type}: ${errors?.name?.message}` },
-    { name: 'email', type: 'text', placeholder: 'Write email', title: 'Email', message: errors?.email && `${errors?.email?.type}: ${errors?.email?.message}` },
+    { name: 'email', type: 'email', placeholder: 'Write email', title: 'Email', message: errors?.email && `${errors?.email?.type}: ${errors?.email?.message}` },
     { name: 'password', type: 'password', placeholder: 'Write password', title: 'Password', message: errors?.password && `${errors?.password?.type}: ${errors?.password?.message}` },
   ]
   return inputs
 }
+
+export const getLoginInputsConfigs = (errors: FieldErrors<LoginFormInputs>) => {
+  const inputs: InputsType<Omit<LoginFormInputs, 'role'>>[] = [
+    { name: 'email', type: 'email', placeholder: 'Write email', title: 'email', message: errors?.email && `${errors?.email?.type}: ${errors?.email?.message}` },
+    { name: 'password', type: 'password', placeholder: 'Write password', title: 'Password', message: errors?.password && `${errors?.password?.type}: ${errors?.password?.message}` },
+  ]
+  return inputs
+}
+
 
