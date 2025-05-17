acda5b5b1807ca1f74207cfc69fdfd68e7126029
sfwnisme <safymo424@gmail.com>
2025-05-18 02:09:23 +0300

    refactor(useGetCurrentUser): change IUser type import dir

--- src/hooks/useGetCurrentUser.tsx
+++ src/hooks/useGetCurrentUser.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { axiosInstance } from '../libs/axios-instance'
-import { IUser } from '../types/types'
 import { USER_ROLES } from '../constraints/constraints'
+import { IUser } from '../features/users/types'
 
 export default function useGetCurrentUser() {
   const currentUser = useQuery<Omit<IUser, 'createdAt'>, Error>({
