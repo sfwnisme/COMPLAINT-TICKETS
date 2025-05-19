13e43a7210e7457b63f5a9321b7dfa795105a2ab
sfwnisme <safymo424@gmail.com>
2025-05-20 02:46:16 +0300

    chore(Comment): restructure the component, and accept only a single prop for comment

--- src/features/tickets/components/comment/Comment.tsx
+++ src/features/tickets/components/comment/Comment.tsx
@@ -4,38 +4,38 @@ import { Case, Shift } from "@sfwnisme/visi"
 import { Avatar } from "../../../../components"
 import useDeleteApiData from "../../../../hooks/use-delete-api-data"
 import useUpdateApiData from "../../../../hooks/use-update-api-data"
-import { IComment } from "../../../../types/ticket.types"
+// import { IComment } from "../../../../types/ticket.types"
 import { formatedDate } from "../../../../libs/formated-date"
 import Loader from "../../../../components/loaders/loader/Loader"
 import Dropdown from "../../../../components/dropdown/Dropdown"
 import List from "../../../../components/list/List"
 import ListItem from "../../../../components/list/ListItem"
 import HelpText from "../../../../components/help-text/HelpText"
+import { IComment } from '../../types'
 
+type Props = {
+  comment: IComment
+}
 export default function Comment({
-  _id = '',
-  author = { _id: "", name: "" },
-  createdAt = "",
-  content = "",
-  isSolution = false
-}: Readonly<Omit<IComment, 'ticket' | 'updatedAt'>>) {
+  comment
+}: Readonly<Props>) {
   const { mutateAsync: deleteComment, isPending: isPendingDelete } = useDeleteApiData({ endpoint: `/comments`, revalidateKey: '/comments' })
-  const { mutateAsync: updateComment, isPending: isPendingMark } = useUpdateApiData({ endpoint: '/comments', revalidateKey: '/comments', id: _id, method: 'patch' })
+  const { mutateAsync: updateComment, isPending: isPendingMark } = useUpdateApiData({ endpoint: '/comments', revalidateKey: '/comments', id: comment._id, method: 'patch' })
   const handleDeleteComment = async () => {
-    await deleteComment(_id)
+    await deleteComment(comment._id)
   }
 
   const markCommentSolution = async () => {
-    await updateComment({ isSolution: !isSolution })
+    await updateComment({ isSolution: !comment.isSolution })
   }
 
   return (
-    <div className={`${Style["ticket-page__comment"]} ${isSolution && Style['ticket-page__comment-marked-as-a-solution']}`} id={_id}>
+    <div className={`${Style["ticket-page__comment"]} ${comment.isSolution && Style['ticket-page__comment-marked-as-a-solution']}`} id={comment._id}>
       <div className={`${Style['ticket-page__commenter']}`}>
-        <Avatar name={author.name} key={author._id} size="sm" />
-        <div className={Style['ticket-page__commenter-info']} id={author._id}>
-          <p className={`${Style['ticket-page__commenter__name']}`}>{author.name}</p>
-          <p className={`${Style['ticket-page__commenter__date']}`}>{formatedDate(createdAt)}</p>
+        <Avatar name={comment.author.name} key={comment.author._id} size="sm" />
+        <div className={Style['ticket-page__commenter-info']} id={comment.author._id}>
+          <p className={`${Style['ticket-page__commenter__name']}`}>{comment.author.name}</p>
+          <p className={`${Style['ticket-page__commenter__date']}`}>{formatedDate(comment.createdAt)}</p>
         </div>
         <Shift >
           <Case when={isPendingMark || isPendingDelete}>
@@ -45,7 +45,7 @@ export default function Comment({
             <Dropdown>
               <List position='absolute' rightOrLeft='right'>
                 <ListItem onClick={markCommentSolution}>
-                  {!isSolution ? 'Mark solution' : 'Unmark solution'}
+                  {!comment.isSolution ? 'Mark solution' : 'Unmark solution'}
                 </ListItem>
                 <ListItem onClick={handleDeleteComment}>
                   Remove
@@ -56,9 +56,9 @@ export default function Comment({
         </Shift>
       </div>
       <p className={`${Style['ticket-page__commenter__message']}`}>
-        {content}
+        {comment.content}
       </p>
-      {isSolution && <HelpText variant='success'>This response marked as a solution</HelpText>}
+      {comment.isSolution && <HelpText variant='success'>This response marked as a solution</HelpText>}
     </div>
   )
 }
