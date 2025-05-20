2dcb8440a7c9027741297ce5ef1de51fc0f2f642
sfwnisme <safymo424@gmail.com>
2025-05-21 01:01:02 +0300

    fix(FloatTicket): add optional chaining to avoid undefined errors

--- src/features/tickets/components/floatTicket/FloatTicket.tsx
+++ src/features/tickets/components/floatTicket/FloatTicket.tsx
@@ -34,7 +34,7 @@ export default function FloatTicket() {
     <div className={Style["float-ticket__overlay"]}>
       <div className={Style['float-ticket']}>
         <FloatTicketHeader title={ticket.title} />
-        <FloatTicketAuthor name={ticket.createdBy.name} createdAt={ticket.createdAt ?? ""} />
+        <FloatTicketAuthor name={ticket?.createdBy?.name} createdAt={ticket.createdAt ?? ""} />
         <FloatTicketInfo
           ticketId={ticket._id}
           department={ticket.department}
