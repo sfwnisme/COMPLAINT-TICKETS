b0892cf6477ebb86ec18517ff2d7fc7641cd1698
sfwnisme <safymo424@gmail.com>
2025-05-20 03:35:06 +0300

    refactor(Tickets): replace title component with page header, and add ticket insights component, and

--- src/app/dashboard/tickets/Tickets.tsx
+++ src/app/dashboard/tickets/Tickets.tsx
@@ -1,15 +1,17 @@
-import Title from '../../../components/title/Title.tsx'
 import FloatTicket from '../../../features/tickets/components/floatTicket/FloatTicket.tsx';
 import { useFloatTicket } from '../../../store/ticket.store.ts';
 import RenderTicketsContent from '../../../features/tickets/components/renderTicketsContent/RenderTicketsContent.tsx';
+import TicketPageInsights from '../../../features/tickets/components/ticketPageInsights/TicketPageInsights.tsx';
+import PageHeader from '../../../components/pageHeader/PageHeader.tsx';
 
 export default function Tickets() {
   const isFloatTicketVisible = useFloatTicket((state) => state.isFloatTicketVisible)
   return (
     <div>
-      {isFloatTicketVisible ? <FloatTicket /> : null}
-      <Title text='Tickets' />
+      <TicketPageInsights />
+      <PageHeader title='Tickets' button='Create Ticket' href='/dashboard/tickets/create' />
       <RenderTicketsContent />
+      {isFloatTicketVisible ? <FloatTicket /> : null}
     </div >
   )
 }
