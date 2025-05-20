8ace82634946160907bc7f4bfa5312cabb3d53d5
sfwnisme <safymo424@gmail.com>
2025-05-21 01:02:51 +0300

    rename(inputsConfig): rename inputsConfigs > inputsConfig

--- src/features/users/inputsConfig.ts
+++ src/features/users/inputsConfig.ts
@@ -1,18 +1,10 @@
-import { FieldErrors, FieldValues, Path } from "react-hook-form";
+import { FieldErrors } from "react-hook-form";
 import { z } from "zod";
-import { createUserSchema, loginSchema, updateUserSchema } from "./schemas/user.schema";
+import { createUserSchema, updateUserSchema } from "./schemas/user.schema";
+import { InputsType } from "../../types/types";
 
-type LoginFormInputs = z.infer<typeof loginSchema>
 type CreateUserFormInputs = z.infer<typeof createUserSchema>;
 type UpdateUserFormInputs = z.infer<typeof updateUserSchema>
-type InputsType<T extends FieldValues> = {
-  name: Path<T>;
-  type: "text" | "email" | "password";
-  placeholder?: string;
-  title?: string;
-  message?: string;
-};
-
 
 /** 
  * @param errors react hook form errors object
@@ -40,12 +32,5 @@ export const getCreateUserInputsConfigs = (errors: FieldErrors<CreateUserFormInp
   return inputs
 }
 
-export const getLoginInputsConfigs = (errors: FieldErrors<LoginFormInputs>) => {
-  const inputs: InputsType<Omit<LoginFormInputs, 'role'>>[] = [
-    { name: 'email', type: 'email', placeholder: 'Write email', title: 'email', message: errors?.email && `${errors?.email?.type}: ${errors?.email?.message}` },
-    { name: 'password', type: 'password', placeholder: 'Write password', title: 'Password', message: errors?.password && `${errors?.password?.type}: ${errors?.password?.message}` },
-  ]
-  return inputs
-}
 
 
