a6155226e0a39430237329a3ea994c48b6a20a0a
sfwnisme <safymo424@gmail.com>
2025-05-20 03:31:59 +0300

    feat(floatTicketAuthor): author section component for float ticket

---
+++ src/features/tickets/components/floatTicketAuthor/FloatTicketAuthor.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Style from './FloatTicketAuthor.module.css'
+import { formatedDate } from '../../../../libs/formated-date'
+import { Avatar } from '../../../../components'
+
+type Props = {
+  name: string,
+  createdAt: string,
+}
+
+export default function FloatTicketAuthor({ name, createdAt }: Props) {
+  return (
+    <div className={Style['float-ticket__createdby']}>
+      <Avatar size='md' name={name} />
+      <div className={Style['float-ticket__createdby-info']}>
+        <p className={`${Style['float-ticket__createdby__name']}`}>{name}</p>
+        <p className={`${Style['float-ticket__createdby__date']}`}>{formatedDate(createdAt)}</p>
+      </div>
+    </div>
+  )
+}
