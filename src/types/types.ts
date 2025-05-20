b71356a19e38344dc50d255b3f981df4712a00cb
sfwnisme <safymo424@gmail.com>
2025-05-20 22:32:14 +0300

    chore(types): gather all the application types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,10 @@
 import { AxiosError } from "axios"
+export type TAxiosError = AxiosError<{ msg: string | { [key: string]: string }[] }>
 
+
+//-----------------------------
+// User
+//-----------------------------
 export interface IUser {
   _id: string,
   name: string,
@@ -8,6 +13,50 @@ export interface IUser {
   createdAt?: string,
   updatedAt?: string,
 }
-export type TAxiosError = AxiosError<{ msg: string | { [key: string]: string }[] }>
-
 export type Role = 'admin' | 'manager' | 'csr' | 'view_only'
+
+//-----------------------------
+// Ticket
+//-----------------------------
+export type Status = "open" | "in-progress" | "resolved" | "closed"
+export type Priority = 'low' | 'medium' | 'high' | 'critical'
+export interface ITag {
+  _id: string,
+  name: string
+  color: string
+}
+export interface IDepartment {
+  _id: string,
+  title: string,
+  createdAt: string,
+  updatedAt: string,
+}
+export interface ITicket {
+  _id: string,
+  title: string,
+  description?: string,
+  status: Status,
+  priority: Priority,
+  createdBy: { _id: string, name: string },
+  tags: ITag[],
+  department: { _id: string, title: string }
+  createdAt?: string,
+  updatedAt?: string
+}
+
+export interface IComment {
+  _id: string,
+  content: string,
+  author: { _id: string, name: string },
+  ticket: { _id: string, title: string },
+  isSolution: boolean,
+  createdAt: string,
+  updatedAt: string,
+}
+
+//-----------------------------
+// Components
+//-----------------------------
+export type Sizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type Variants = 'primary' | 'info' | 'warning' | 'success' | 'danger';
+
