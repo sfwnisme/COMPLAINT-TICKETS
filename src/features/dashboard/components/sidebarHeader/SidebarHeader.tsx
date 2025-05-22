438a30570a831c65a619773be780f9acef81988f
sfwnisme <safymo424@gmail.com>
2025-05-23 01:37:24 +0300

    feat(sidebarHeader): side bar header component

---
+++ src/features/dashboard/components/sidebarHeader/SidebarHeader.tsx
@@ -0,0 +1,17 @@
+import Style from './SidebarHeader.module.css'
+import Logo from '../../../../assets/logo.png'
+import { useDashboard } from '../../../../store/dashboard.store'
+import Button from '../../../../components/button/Button'
+import {  X } from 'lucide-react'
+
+export default function SidebarHeader() {
+  const { toggleSidebar } = useDashboard((state) => state)
+  return (
+    <div className={Style["sidebar__header"]}>
+      <img className={Style['sidebar__logo']} src={Logo} alt="" />
+      <Button onClick={toggleSidebar} size='square' shape='none'>
+        <X size={25} />
+      </Button>
+    </div>
+  )
+}
