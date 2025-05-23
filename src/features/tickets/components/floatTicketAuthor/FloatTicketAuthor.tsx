159f5f7b814df35c61da3d08658b502817e6b76d
sfwnisme <safymo424@gmail.com>
2025-05-23 16:31:16 +0300

    refactor(floatTicketAuthor): replace redundant code with UserChip, and UserChipSkeleton components

--- src/features/tickets/components/floatTicketAuthor/FloatTicketAuthor.tsx
+++ src/features/tickets/components/floatTicketAuthor/FloatTicketAuthor.tsx
@@ -1,20 +1,11 @@
-import Style from './FloatTicketAuthor.module.css'
+import UserChip from '../../../../components/userChip/UserChip'
 import { formatedDate } from '../../../../libs/formated-date'
-import { Avatar } from '../../../../components'
 
 type Props = {
   name: string,
   createdAt: string,
 }
 
-export default function FloatTicketAuthor({ name, createdAt }: Props) {
-  return (
-    <div className={Style['float-ticket__createdby']}>
-      <Avatar size='md' name={name} />
-      <div className={Style['float-ticket__createdby-info']}>
-        <p className={`${Style['float-ticket__createdby__name']}`}>{name}</p>
-        <p className={`${Style['float-ticket__createdby__date']}`}>{formatedDate(createdAt)}</p>
-      </div>
-    </div>
-  )
+export default function FloatTicketAuthor({ name, createdAt }: Readonly<Props>) {
+  return <UserChip name={name} text={formatedDate(createdAt)} avatarSize='md' />
 }
--- src/features/tickets/components/floatTicketAuthor/FloatTicketAuthorSkeleton.tsx
+++ src/features/tickets/components/floatTicketAuthor/FloatTicketAuthorSkeleton.tsx
@@ -1,16 +1,5 @@
-import Style from './FloatTicketAuthor.module.css'
-import Skeleton from '../../../../components/skeleton/Skeleton'
-import Spacer from '../../../../components/spacer/Spacer'
+import UserChipSkeleton from '../../../../components/userChip/UserChipSkeleton'
 
 export default function FloatTicketAuthorSkeleton() {
-  return (
-    <div className={Style['float-ticket__createdby']}>
-      <Skeleton width='40px' height='40px' />
-      <div className={Style['float-ticket__createdby-info']}>
-        <Skeleton height='15px' width='20%' />
-        <Spacer />
-        <Skeleton height='12px' width='30%' />
-      </div>
-    </div>
-  )
+  return <UserChipSkeleton />
 }
