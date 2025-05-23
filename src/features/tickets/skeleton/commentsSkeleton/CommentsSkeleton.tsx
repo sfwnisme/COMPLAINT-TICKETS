9bb61b8166cee687975165ede0a7a0f129886159
sfwnisme <safymo424@gmail.com>
2025-05-23 03:45:31 +0300

    remove(skeleton): deleted tickets skeleton dir

--- src/features/tickets/skeleton/commentsSkeleton/CommentsSkeleton.tsx
+++
@@ -1,6 +0,0 @@
-import CommentSkeleton from '../comment/CommentSkeleton'
-
-export default function CommentsSkeleton() {
-  const arr = [1, 3, 4, 234]
-  return arr.map((a) => <CommentSkeleton key={a} />)
-}
