caf656b632359f16488a150231b68dbcac05e0f3
sfwnisme <safymo424@gmail.com>
2025-05-23 03:31:05 +0300

    refactor(Comments): remove skeleton component

--- src/features/tickets/components/comments/Comments.tsx
+++ src/features/tickets/components/comments/Comments.tsx
@@ -2,8 +2,8 @@ import Comment from '../comment/Comment'
 import useGetArrayByIdApiData from '../../../../hooks/use-get-api-data-by-query'
 import Alert from '../../../../components/alert/Alert'
 import Style from './Comments.module.css'
-import CommentsSkeleton from '../../skeleton/commentsSkeleton/CommentsSkeleton'
 import { IComment } from '../../../../types/types'
+import CommentSkeleton from '../comment/CommentSkeleton'
 
 type Props = {
   ticketId: string
@@ -13,7 +13,7 @@ export default function Comments({ ticketId }: Props) {
   const { data: comments, isLoading } = useGetArrayByIdApiData<IComment[]>({ endpoint: '/comments', query: `?ticketId=${ticketId}` })
   console.log('comments', comments)
   if (comments?.length === 0) return
-  if (isLoading) return <CommentsSkeleton />
+  if (isLoading) return <CommentSkeleton />
 
   const renderCommentsList = comments?.map((comment: IComment) => {
     return (
