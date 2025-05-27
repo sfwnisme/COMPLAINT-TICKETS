6943904f66521b0da71c7c049613455e0b36a346
sfwnisme <safymo424@gmail.com>
2025-05-28 02:32:53 +0300

    feat(userChip): add fontSize prop, and show text conditionally

--- src/components/userChip/UserChip.tsx
+++ src/components/userChip/UserChip.tsx
@@ -4,17 +4,25 @@ import { Sizes } from '../../types/types'
 
 type Props = {
   name: string,
-  text: string,
+  text?: string,
   avatarSize?: Sizes,
+  fontSize?: Sizes
 }
 
-export default function UserChip({ name = 'user', text = " date or email", avatarSize = 'sm' }: Readonly<Props>) {
+export default function UserChip({ name = 'user', text = "", avatarSize = 'sm', fontSize = "xs" }: Readonly<Props>) {
+  const sizes = {
+    xs: Style['user-chip__name--xs'],
+    sm: Style['user-chip__name--sm'],
+    md: Style['user-chip__name--md'],
+    lg: Style['user-chip__name--lg'],
+    xl: Style['user-chip__name--xl'],
+  }
   return (
     <div className={Style['user-chip']}>
       <Avatar size={avatarSize} name={name} />
       <div className={Style['user-chip__info']}>
-        <p className={`${Style['user-chip__name']}`}>{name}</p>
-        <p className={`${Style['user-chip__text']}`}>{text}</p>
+        <p className={`${Style['user-chip__name']} ${sizes[fontSize]}`}>{name}</p>
+        {text && <p className={`${Style['user-chip__text']}`}>{text}</p>}
       </div>
     </div>
   )
