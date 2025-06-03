af65d81b0b891bc7e205ee1193fb978f68bbfa2e
sfwnisme <safymo424@gmail.com>
2025-06-03 03:52:00 +0300

    feat(use-create-ticket): create ticket request hook

---
+++ src/features/tickets/hooks/use-create-ticket.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import useCreateApiData from '../../../hooks/use-create-api-data'
+import { SubmitHandler } from 'react-hook-form';
+import { useCallback } from 'react';
+import { CreateTicketType } from '../schemas/ticket.schema';
+
+export default function useCreateTicket() {
+  const { mutateAsync, isPending, isSuccess, isError, error } = useCreateApiData<CreateTicketType>({ endpoint: "/tickets/create", revalidateKey: "/create" })
+
+  const onSubmit: SubmitHandler<CreateTicketType> = useCallback(async (data) => {
+    try {
+      const res = await mutateAsync(data);
+      return res;
+    } catch (error) {
+      console.error("create ticket error", error);
+    }
+  }, [mutateAsync])
+
+  let typedError;
+  if (axios.isAxiosError(error)) {
+    typedError = error;
+  }
+  const errorMessage = Array.isArray(typedError?.response?.data?.msg)
+    ? typedError?.response?.data?.msg.map((m: { path: string, msg: string }) => m.path + ":" + m.msg)
+    : typedError?.response?.data?.msg
+  const successMessage = 'The ticket created successfull, you can now log in to activeate the account';
+
+  return { onSubmit, isPending, isSuccess, isError, errorMessage, successMessage }
+}
