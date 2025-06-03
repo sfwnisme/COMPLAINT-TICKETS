745e7c61a2804954d907c258ccb16b33f2cd6859
sfwnisme <safymo424@gmail.com>
2025-06-03 03:47:44 +0300

    feat(selectTags): select input for tags

---
+++ src/features/tickets/forms/selectTags/SelectTags.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Select from '../../../../components/select/Select'
+import useGetUsers from '../../../users/hooks/use-get-users'
+import useGetTags from '../../../../hooks/use-get-tags'
+
+type Props = {
+  title: string,
+} & React.DetailedHTMLProps<
+  React.SelectHTMLAttributes<HTMLSelectElement>,
+  HTMLSelectElement>
+const SelectTags = React.forwardRef<HTMLSelectElement, Props>(
+  ({ title, ...rest }, ref) => {
+    const { data } = useGetTags()
+    console.log(data)
+    const tags = data ?? []
+
+    return (
+      <Select ref={ref} title={title} {...rest} >
+        <option disabled value="">
+          Select User
+        </option>
+        {tags.map((user) => (
+          <option key={user?._id} value={user?._id} id={user?._id}>
+            {user?.name}
+          </option>
+        ))}
+      </Select>
+    )
+  }
+)
+export default SelectTags
