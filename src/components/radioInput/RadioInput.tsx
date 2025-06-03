cee7da02073f36e54f073c3a02e54ffe55c05d9b
sfwnisme <safymo424@gmail.com>
2025-06-03 03:46:14 +0300

    feat(radioInput): radio input

---
+++ src/components/radioInput/RadioInput.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Style from './RadioInput.module.css'
+
+type Props = {
+  id: string,
+  value: string,
+  label: string,
+} & React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+const RadioInput = React.forwardRef<HTMLInputElement, Props>(({ id, label, ...rest }, ref) => {
+  return (
+    <div className={Style['radio-input-container']}>
+      <input className={Style['radio-input']} {...rest} type='radio' value={rest.value} id={id} ref={ref} hidden />
+      <label htmlFor={id} className={Style['radio-input__label']}>{label}</label>
+    </div>
+  )
+})
+
+export default RadioInput
