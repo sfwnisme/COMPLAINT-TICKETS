7675b4addc907bdb4ded6f47281856b7008d1702
sfwnisme <safymo424@gmail.com>
2025-06-05 02:14:38 +0300

    feat(App): apply react query devtools component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useEffect } from 'react'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
-useEffect(() => {
-  document.title = 'TIC'
-})
+  useEffect(() => {
+    document.title = 'TIC'
+  })
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
