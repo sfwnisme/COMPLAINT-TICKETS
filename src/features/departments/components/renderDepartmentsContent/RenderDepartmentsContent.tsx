9c510325342b9126528521e59ec1ef1f183298ec
sfwnisme <safymo424@gmail.com>
2025-06-04 06:11:23 +0300

    feat(RenderDepartmentsContent): departments content component

---
+++ src/features/departments/components/renderDepartmentsContent/RenderDepartmentsContent.tsx
@@ -0,0 +1,25 @@
+import React, { useMemo } from 'react'
+import useGetDepartments from '../../hooks/use-get-departments'
+import LoadingDepartments from '../loadingDepartments/LoadingDepartments'
+import RenderDepartments from '../renderDepartments/RenderDepartments'
+import ErrorDepartments from '../errorDepartments/ErrorDepartments'
+import NoDepartments from '../noDepartments/NoDepartments'
+
+export default function RenderDepartmentsContent() {
+  const departments = useGetDepartments()
+  console.log('departments', departments)
+  const content = useMemo(() => {
+    if (departments.isLoading) return <LoadingDepartments />
+    if (departments.isError) return <ErrorDepartments error={departments.error.response?.data.msg ?? ''} />
+    if (departments.data?.length === 0) return <NoDepartments />
+    if (departments.isSuccess) return <RenderDepartments departments={departments?.data} />
+  }, [
+    departments.data,
+    departments.isLoading,
+    departments.isSuccess,
+    departments.isError,
+    departments.error
+  ])
+
+  return content
+}
