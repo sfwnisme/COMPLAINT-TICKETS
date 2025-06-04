848dcf8035797513bc4f95416a3f9354480ef393
sfwnisme <safymo424@gmail.com>
2025-06-04 06:13:17 +0300

    feat(Departments): create departments page

--- src/app/dashboard/departments/Departments.tsx
+++ src/app/dashboard/departments/Departments.tsx
@@ -1,5 +1,14 @@
+import PageHeader from "../../../components/pageHeader/PageHeader";
+import LoadingDepartments from "../../../features/departments/components/loadingDepartments/LoadingDepartments";
+// import RenderDepartments from "../../../features/departments/components/renderDepartments/RenderDepartments";
+import RenderDepartmentsContent from "../../../features/departments/components/renderDepartmentsContent/RenderDepartmentsContent";
+
 export default function Departments() {
   return (
-    <div>Departments</div>
+    <div>
+      <PageHeader title="Departments" button="Create Department" />
+      {/* <LoadingDepartments /> */}
+      <RenderDepartmentsContent/>
+    </div>
   )
 }
