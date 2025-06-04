29fbc951ab771fca561d4daa3504efa82b57f6b7
sfwnisme <safymo424@gmail.com>
2025-06-04 13:52:52 +0300

    feat(Home): add HomeLayout wrapper component

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+import HomeLayout from "../../features/home/homeLayout/HomeLayout";
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <HomeLayout>
+      <Outlet />
+    </HomeLayout>
   )
 }
