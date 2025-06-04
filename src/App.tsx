932ff827396e10ad38929b091a55bae9c9172948
sfwnisme <safymo424@gmail.com>
2025-06-04 13:31:40 +0300

    feat(App): change app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 function App() {
-
+useEffect(() => {
+  document.title = 'TIC'
+})
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
