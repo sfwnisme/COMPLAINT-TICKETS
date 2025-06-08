79db7b27b6757d4b914f012104b10f646fecd055
sfwnisme <safymo424@gmail.com>
2025-06-08 17:25:29 +0300

    feat(createTagForm): create new tag form

---
+++ src/features/tags/components/createTagForm/CreateTagForm.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import Button from "../../../../components/button/Button";
+import Input from "../../../../components/input/Input";
+import LoadingIcon from "../../../../components/loadingIcon/LoadingIcon";
+// import useCreateTag from "../../hooks/use-create-tag";
+// import useCreateTagFormValidation from "../../hooks/use-create-tag-form-validation";
+import { Plus, X } from "lucide-react";
+import Spacer from "../../../../components/spacer/Spacer";
+import useCreateTag from "../../hooks/use-create-tag";
+import useCreateTagFormValidation from "../../hooks/use-create-tag-form-validation";
+
+export default function CreateTagForm() {
+  const [activeCreate, setActiveCreate] = useState(false)
+  const { register, handleSubmit, formState: { errors, isValid } } = useCreateTagFormValidation()
+  const { onSubmit, isPending, isSuccess, errorMessage } = useCreateTag()
+  const canCreate = !isPending && isValid
+
+  useEffect(() => {
+    setActiveCreate(false)
+  }, [isSuccess])
+
+  return (
+    <div>
+      <Spacer />
+      <div style={{ display: "flex", flexWrap: 'wrap', gap: '3px', alignItems: 'start', justifyContent: 'end' }}>
+        {activeCreate ?
+          <>
+            <form id="creaet_tag_form" onSubmit={handleSubmit(onSubmit)} />
+            <Input type='text' form='creaet_tag_form' {...register('name')}
+              message={errors?.name?.message ?? errorMessage}
+              variant={errors?.name?.message || errorMessage ? 'danger' : 'primary'}
+            />
+            <Button form='creaet_tag_form' disabled={!canCreate}>
+              {isPending && <LoadingIcon />}
+              create
+            </Button>
+            <Button variant="danger"  onClick={() => setActiveCreate(false)}><X size={15} /> Close</Button>
+          </>
+          : <Button onClick={() => setActiveCreate(true)}><Plus size={15} /> New</Button>
+        }
+      </div>
+    </div>
+  )
+}
