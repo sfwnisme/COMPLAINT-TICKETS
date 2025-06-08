83f6f5af1b48070ec239c668485c44928a7d7964
sfwnisme <safymo424@gmail.com>
2025-06-08 17:15:32 +0300

    feat(Tags): tags page

---
+++ src/app/dashboard/tags/Tags.tsx
@@ -0,0 +1,11 @@
+import PageHeader from "../../../components/pageHeader/PageHeader";
+import RenderTagsContent from "../../../features/tags/components/renderDepartmentsContent/RenderTagsContent";
+
+export default function Tags() {
+  return (
+    <div>
+      <PageHeader title="Tags" />
+      <RenderTagsContent />
+    </div>
+  )
+}
