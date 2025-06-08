c93d28b56687bff9548d95bf3f2796cf78d80890
sfwnisme <safymo424@gmail.com>
2025-06-08 14:28:40 +0300

    feat(department.schema): create department schema

--- src/features/departments/schemas/department.schema.ts
+++ src/features/departments/schemas/department.schema.ts
@@ -3,5 +3,9 @@ import { z } from "zod";
 export const updateDepartmentSchema = z.object({
   title: z.string().min(5, 'department name must be at least 3 characters')
 })
+export const createDepartmentSchema = z.object({
+  title: z.string().min(5, 'department name must be at least 3 characters')
+})
 
 export type UpdateDepartmentSchemaType = z.infer<typeof updateDepartmentSchema>
+export type CreateDepartmentSchemaType = z.infer<typeof createDepartmentSchema>
