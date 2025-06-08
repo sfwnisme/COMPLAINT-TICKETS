1390e197ba826e6fd6d01588615c74229b753d3e
sfwnisme <safymo424@gmail.com>
2025-06-08 17:20:19 +0300

    feat(use-update-tag-form-validation): update tag custom form validation hook

---
+++ src/features/tags/hooks/use-update-tag-form-validation.ts
@@ -0,0 +1,20 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import useGetTag from './use-get-tag';
+import { updateTagSchema, UpdateTagSchemaType } from '../schemas/tag.schema';
+// import { updateTagSchema, UpdateTagSchemaType } from '../schemas/tag.schema';
+
+export default function useUpdateTagFormValidation(tagId: string = '') {
+  const { data: tag, isFetched } = useGetTag(tagId)
+  const form = useForm<UpdateTagSchemaType>({
+    resolver: zodResolver(updateTagSchema),
+    mode: 'all',
+    values: {
+      name: tag?.name ?? 'loading...'
+    },
+  });
+  if (isFetched) {
+    form.setFocus('name')
+  }
+  return form
+}
