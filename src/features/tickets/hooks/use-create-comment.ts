8db6077b1467195e606defe83e48fa7e652ac7fa
sfwnisme <safymo424@gmail.com>
2025-06-14 03:04:37 +0300

    feat(use-create-comment): return formated error and success messages

--- src/features/tickets/hooks/use-create-comment.ts
+++ src/features/tickets/hooks/use-create-comment.ts
@@ -3,10 +3,11 @@ import useCreateApiData from '../../../hooks/use-create-api-data'
 import { SubmitHandler } from 'react-hook-form'
 import { useCallback } from 'react'
 import { createCommentSchema } from '../schemas/ticket.schema'
+import axios from 'axios'
 
 type Inputs = z.infer<typeof createCommentSchema>
 export default function useCreateComment(ticketId: string = "") {
-  const { mutateAsync, isPending, isSuccess, isError } = useCreateApiData<Inputs>({ endpoint: '/comments/create', revalidateKey: [['/comments'], ['/tickets', ticketId]] })
+  const createComment = useCreateApiData<Inputs>({ endpoint: '/comments/create', revalidateKey: [['/comments'], ['/tickets', ticketId]] })
 
   const onSubmit: SubmitHandler<Inputs> = useCallback(async (data, e) => {
     const newComment = {
@@ -15,14 +16,24 @@ export default function useCreateComment(ticketId: string = "") {
       isSolution: false,
     }
     try {
-      const res = await mutateAsync(newComment)
+      const res = await createComment.mutateAsync(newComment)
       console.log('success comment')
       e?.target.reset()
       return res
     } catch (error) {
       console.log('new comment error', error)
     }
-  }, [mutateAsync])
-  return { onSubmit: onSubmit, isPending, isSuccess, isError }
+  }, [createComment.mutateAsync])
+  let typedError;
+  if (axios.isAxiosError(createComment.error)) {
+    typedError = createComment.error;
+  }
+  const errorMessage = Array.isArray(typedError?.response?.data?.msg)
+    ? typedError?.response?.data?.msg.map((m: { path: string, msg: string }) => m.path + ":" + m.msg)
+    : typedError?.response?.data?.msg
+  const successMessage = 'The ticket created successfull, you can now log in to activeate the account';
+
+
+  return { onSubmit: onSubmit,errorMessage, successMessage, ...createComment }
 
 }
