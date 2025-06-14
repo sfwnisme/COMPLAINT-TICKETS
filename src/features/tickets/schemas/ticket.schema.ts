14a1667cca93060e9b0b0661dcfc6fe8bf100007
sfwnisme <safymo424@gmail.com>
2025-06-15 02:38:44 +0300

    feat(ticket.schema): update ticket zod schema

--- src/features/tickets/schemas/ticket.schema.ts
+++ src/features/tickets/schemas/ticket.schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { TICKET_PRIORITY } from "../../../constraints/constraints";
+import { TICKET_PRIORITY, TICKET_STATUS } from "../../../constraints/constraints";
 
 export const createCommentSchema = z.object({
   content: z.string().min(3, 'name should be at least 3 characters').max(3000, 'name should be maximum 3000 characters '),
@@ -8,10 +8,15 @@ export const createCommentSchema = z.object({
 export const createTicketSchema = z.object({
   title: z.string().min(15, 'ticket title must be 15 characters at least').max(120, 'ticket title must be 120 characters maximum'),
   description: z.string().min(15, 'ticket title must be 15 characters at least'),
-  // status: z.enum(TICKET_STATUS),
   priority: z.enum(TICKET_PRIORITY),
-  // createdBy: z.string().optional(),
   assignedTo: z.string().optional(),
   tags: z.string().array().optional()
 })
 export type CreateTicketType = z.infer<typeof createTicketSchema>
+
+export const updateTicketSchema = z.object({
+  status: z.enum(TICKET_STATUS).optional(),
+  assignedTo: z.string().optional(),
+  tags: z.string().array().optional()
+})
+export type UpdateTicketType = z.infer<typeof updateTicketSchema>
