27da5c468da5c4f1b7dc91b3b9300320488e2726
sfwnisme <safymo424@gmail.com>
2025-06-18 01:00:26 +0300

    feat(can): permission component wrapper

---
+++ src/components/can/Can.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import useGetCurrentUser from '../../hooks/useGetCurrentUser'
+import { Permission, Routes } from '../../types/types'
+import { PERMISSIONS } from '../../constraints/permissions';
+
+type Props = {
+  permission: keyof Permission;
+  route: Routes;
+  children: React.ReactNode
+}
+
+export default function Can({ permission, route, children }: Readonly<Props>) {
+  const currentUser = useGetCurrentUser()
+  if (!currentUser?.data) {
+    return null
+  }
+  const role = currentUser.data.role
+
+  const hasAccess = PERMISSIONS[role][route][permission]
+
+  if (!hasAccess) {
+    return null
+  }
+
+  return <>{children}</>
+}
