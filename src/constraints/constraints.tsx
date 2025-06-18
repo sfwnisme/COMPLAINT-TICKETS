961705596458ea12f51abb784a394a4dd2f075dd
sfwnisme <safymo424@gmail.com>
2025-06-19 00:43:44 +0300

    refactor(constraints): use lower case for USER_ROLES keys, and strongly define the types

--- src/constraints/constraints.tsx
+++ src/constraints/constraints.tsx
@@ -1,11 +1,11 @@
 import { Role, Variants } from "../types/types"
 
 // USER----------------------
-export const USER_ROLES: { [key: string]: Role } = {
-  ADMIN: 'admin',
-  MANAGER: 'manager',
-  CSR: 'csr',
-  VIEW_ONLY: 'view_only',
+export const USER_ROLES: Record<Role, Role> = {
+  admin: 'admin',
+  manager: 'manager',
+  csr: 'csr',
+  view_only: 'view_only',
 }
 export const USER_ROLES_VALUES = ['admin', 'manager', 'csr', 'view_only'] as const
 export const USER_ROLES_COLORS: Record<string, Variants> = {
@@ -17,7 +17,7 @@ export const USER_ROLES_COLORS: Record<string, Variants> = {
 // END USER----------------------
 
 // TICKET----------------------
-export const TICKET_STATUS= ['open', 'in-progress', 'resolved', 'closed'] as const
+export const TICKET_STATUS = ['open', 'in-progress', 'resolved', 'closed'] as const
 export const TICKET_STATUS_COLORS: Record<string, Variants> = {
   open: 'primary',
   'in-progress': 'info',
