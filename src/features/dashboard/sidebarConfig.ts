42252914b2ec0bb7992813510d6871e26587d845
sfwnisme <safymo424@gmail.com>
2025-06-19 01:25:59 +0300

    feat(sidebarConfig): implement the permission inside the path object

--- src/features/dashboard/sidebarConfig.ts
+++ src/features/dashboard/sidebarConfig.ts
@@ -10,6 +10,8 @@ import {
   UserPlus,
   Users
 } from "lucide-react"
+import { USER_ROLES } from "../../constraints/constraints"
+import { RouteConfig } from "../../types/types"
 let id = 0
 
 export const sidebarSettingPagesLinks = [
@@ -17,13 +19,13 @@ export const sidebarSettingPagesLinks = [
   { id: id++, icon: Settings, title: 'settings', path: '/dashboard/profile/settings' },
 ]
 
-export const sidebarPagesLinks = [
-  { id: id++, icon: Home, title: 'home', path: '/dashboard' },
-  { id: id++, icon: Tickets, title: 'tickets', path: '/dashboard/tickets' },
-  { id: id++, icon: Ticket, title: 'create ticket', path: '/dashboard/tickets/create' },
-  { id: id++, icon: Blocks, title: 'departments', path: '/dashboard/departments' },
-  { id: id++, icon: Ungroup, title: 'tags', path: '/dashboard/tags' },
-  { id: id++, icon: ChartPie, title: 'metrics', path: '/dashboard/metrics' },
-  { id: id++, icon: Users, title: 'users', path: '/dashboard/users' },
-  { id: id++, icon: UserPlus, title: 'create user', path: '/dashboard/users/create' },
+export const sidebarPagesLinks: RouteConfig[] = [
+  { id: id++, icon: Home, title: 'home', path: '/dashboard', permission: [...Object.values(USER_ROLES)] },
+  { id: id++, icon: Tickets, title: 'tickets', path: '/dashboard/tickets', permission: [...Object.values(USER_ROLES)] },
+  { id: id++, icon: Ticket, title: 'create ticket', path: '/dashboard/tickets/create', permission: [USER_ROLES.admin, USER_ROLES.manager, USER_ROLES.csr] },
+  { id: id++, icon: Blocks, title: 'departments', path: '/dashboard/departments', permission: [...Object.values(USER_ROLES)] },
+  { id: id++, icon: Ungroup, title: 'tags', path: '/dashboard/tags', permission: [...Object.values(USER_ROLES)] },
+  { id: id++, icon: ChartPie, title: 'metrics', path: '/dashboard/metrics', permission: [...Object.values(USER_ROLES)] },
+  { id: id++, icon: Users, title: 'users', path: '/dashboard/users', permission: [...Object.values(USER_ROLES)] },
+  { id: id++, icon: UserPlus, title: 'create user', path: '/dashboard/users/create', permission: [USER_ROLES.admin, USER_ROLES.manager, USER_ROLES.csr] },
 ]
