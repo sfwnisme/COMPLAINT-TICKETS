c39b1e313075d12e04e432bcc19b5ad96fe28675
sfwnisme <safymo424@gmail.com>
2025-06-20 02:29:39 +0300

    feat(floatTicketAuthor): apply new layout and skeleton

--- src/features/tickets/components/floatTicketAuthor/FloatTicketAuthor.tsx
+++ src/features/tickets/components/floatTicketAuthor/FloatTicketAuthor.tsx
@@ -1,11 +1,27 @@
 import UserChip from '../../../../components/userChip/UserChip'
 import { formateDate } from '../../../../libs/formate-date'
+import Button from '../../../../components/button/Button'
+import Style from './FloatTicketAuthor.module.css'
+import { NavLink } from 'react-router-dom'
 
 type Props = {
+  ticketId: string,
   name: string,
   createdAt: string,
 }
 
-export default function FloatTicketAuthor({ name, createdAt }: Readonly<Props>) {
-  return <UserChip name={name} text={formateDate(createdAt)} avatarSize='md' fontSize='lg' />
+export default function FloatTicketAuthor({ ticketId, name, createdAt }: Readonly<Props>) {
+
+  return (
+    <div className={Style['float-ticket-author']}>
+      <div className={Style['float-ticket-author__author']}>
+        <UserChip name={name} text={formateDate(createdAt)} avatarSize='md' fontSize='lg' />
+      </div>
+      <div className={Style['float-ticket-author__button']}>
+        <NavLink to={'/dashboard/tickets/' + ticketId}>
+          <Button variant='info' size='xs' width="fill" shape='soft' >Open page</Button>
+        </NavLink>
+      </div>
+    </div>
+  )
 }
--- src/features/tickets/components/floatTicketAuthor/FloatTicketAuthorSkeleton.tsx
+++ src/features/tickets/components/floatTicketAuthor/FloatTicketAuthorSkeleton.tsx
@@ -1,5 +1,16 @@
+import Skeleton from '../../../../components/skeleton/Skeleton'
 import UserChipSkeleton from '../../../../components/userChip/UserChipSkeleton'
+import Style from './FloatTicketAuthor.module.css'
 
 export default function FloatTicketAuthorSkeleton() {
-  return <UserChipSkeleton />
+  return (
+    <div className={Style['float-ticket-author']}>
+      <div className={Style['float-ticket-author__author']}>
+        <UserChipSkeleton />
+      </div>
+      <div className={Style['float-ticket-author__button']}>
+        <Skeleton height='20px' width='80px' />
+      </div>
+    </div>
+  )
 }
