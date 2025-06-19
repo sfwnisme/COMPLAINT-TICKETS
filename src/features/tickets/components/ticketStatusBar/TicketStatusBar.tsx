2527f7be904778cec428b0c35bd57d6fdd76138d
sfwnisme <safymo424@gmail.com>
2025-06-20 02:28:11 +0300

    refactor(TicketStatusBar): remove edit button

--- src/features/tickets/components/ticketStatusBar/TicketStatusBar.tsx
+++ src/features/tickets/components/ticketStatusBar/TicketStatusBar.tsx
@@ -1,8 +1,7 @@
-import Button from '../../../../components/button/Button'
 import { TICKET_STATUS } from '../../../../constraints/constraints'
 import { Status } from '../../../../types/types'
 import Style from './TicketStatusBar.module.css'
-import { Check, Edit } from 'lucide-react'
+import { Check} from 'lucide-react'
 import { useCallback } from 'react'
 
 type Props = {
@@ -28,7 +27,6 @@ export default function StatusBar({ currenStatus }: Readonly<Props>) {
   return (
     <div className={Style['status-bar']}>
       {statusList}
-      <Button variant='primary' size='square' width="fill" ><Edit size={13} /></Button>
     </div>
   )
 }
