dec165a2cf39ccd44af02a80af1eb4b9214b51d0
sfwnisme <safymo424@gmail.com>
2025-06-20 04:12:48 +0300

    fix(TicketSkeleton): match skeleton loading by ticket ui

--- src/features/tickets/components/ticket/TicketSkeleton.tsx
+++ src/features/tickets/components/ticket/TicketSkeleton.tsx
@@ -1,6 +1,5 @@
 import S from './Ticket.module.css'
 import Skeleton from '../../../../components/skeleton/Skeleton'
-import Spacer from '../../../../components/spacer/Spacer'
 
 export default function TicketSkeleton() {
   return (
@@ -13,40 +12,29 @@ export default function TicketSkeleton() {
         </div>
         <h3 className={S['ticket__title']}>
           <Skeleton height='19px' />
-          <Spacer />
-          <Skeleton height='19px' width='20%' />
         </h3>
         <Skeleton width='40px' height='19px' />
-        <div className={S["ticket__header__footer"]}>
-          <p className={S["ticket__number"]}>
-            <Skeleton height='15px' width='15px' />
-            <Skeleton width='100px' height='16px' /></p>
-          <div className={S["ticket__assignee"]}>
-            <Skeleton height='15px' width='15px' />
-            <Skeleton width='120px' height='16px' />
-          </div>
-          <div className={S["ticket__created-at"]} >
-            <Skeleton height='15px' width='15px' />
-            <Skeleton width='180px' height='16px' /></div>
-        </div >
       </div >
+      <div className={S["ticket__body"]}>
+        <Skeleton height='15px' width='80px' />
+        <Skeleton height='15px' width='90px' />
+        <Skeleton height='15px' width='120px' />
+        <Skeleton height='15px' width='150px' />
+      </div>
       <div className={S["ticket__footer"]}>
         <div className={S["ticket__footer--left-side"]}>
           <div className={S["ticket__department"]}>
-            <Skeleton width='30px' height='16px' />
+            <Skeleton width='70px' height='16px' />
           </div>
           <div className={S["ticket__priority"]}>
-            <Skeleton width='30px' height='16px' />
+            <Skeleton width='50px' height='16px' />
           </div>
           <div className={S["ticket__status"]}>
-            <Skeleton width='30px' height='16px' />
+            <Skeleton width='40px' height='16px' />
           </div>
         </div>
-        <div className={S["ticket__tags"]}>
-          <Skeleton width='50px' height='16px' />
-          <Skeleton width='40px' height='16px' />
-          <Skeleton width='70px' height='16px' />
-        </div>
+        <Skeleton width='20px' height='20px' />
+        <Skeleton width='50px' height='16px' />
       </div>
     </div >
   )
