aecf569e7cec3f838cdefd1cf5c900029eae88cf
sfwnisme <safymo424@gmail.com>
2025-06-27 00:04:30 +0300

    fix(sidebarConfig): inactive metrics route

--- src/features/dashboard/sidebarConfig.ts
+++ src/features/dashboard/sidebarConfig.ts
@@ -1,6 +1,6 @@
 import {
   Blocks,
-  ChartPie,
+  // ChartPie,
   Home,
   Settings,
   Ticket,
@@ -25,7 +25,7 @@ export const sidebarPagesLinks: RouteConfig[] = [
   { id: id++, icon: Ticket, title: 'create ticket', path: '/dashboard/tickets/create', permission: [USER_ROLES.admin, USER_ROLES.manager, USER_ROLES.csr] },
   { id: id++, icon: Blocks, title: 'departments', path: '/dashboard/departments', permission: [...Object.values(USER_ROLES)] },
   { id: id++, icon: Ungroup, title: 'tags', path: '/dashboard/tags', permission: [...Object.values(USER_ROLES)] },
-  { id: id++, icon: ChartPie, title: 'metrics', path: '/dashboard/metrics', permission: [...Object.values(USER_ROLES)] },
+  // { id: id++, icon: ChartPie, title: 'metrics', path: '/dashboard/metrics', permission: [...Object.values(USER_ROLES)] },
   { id: id++, icon: Users, title: 'users', path: '/dashboard/users', permission: [...Object.values(USER_ROLES)] },
   { id: id++, icon: UserPlus, title: 'create user', path: '/dashboard/users/create', permission: [USER_ROLES.admin, USER_ROLES.manager, USER_ROLES.csr] },
 ]
