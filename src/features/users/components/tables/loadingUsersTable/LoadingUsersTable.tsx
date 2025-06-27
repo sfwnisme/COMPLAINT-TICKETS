4a4933dce6c967a6262e8697b82d21f8251f3d6a
sfwnisme <safymo424@gmail.com>
2025-06-28 02:16:03 +0300

    feat(LoadingUsersTable): apply skeleton loading

--- src/features/users/components/tables/loadingUsersTable/LoadingUsersTable.tsx
+++ src/features/users/components/tables/loadingUsersTable/LoadingUsersTable.tsx
@@ -1,14 +1,54 @@
 import { memo } from 'react'
 import TD from '../../../../../components/table/TD'
 import TR from '../../../../../components/table/TR'
+import Skeleton from '../../../../../components/skeleton/Skeleton'
 
 const LoadingUsersTable = () => {
   return (
-    <TR>
-      <TD colSpan={12} align="center" >
-        loading...
-      </TD>
-    </TR>
+    <>
+      <TR>
+        <TD align="center" >
+          <Skeleton />
+        </TD>
+        <TD align="center" >
+          <Skeleton />
+        </TD>
+        <TD align="center" >
+          <Skeleton width='100px' />
+        </TD>
+        <TD align="center" >
+          <Skeleton height='20px' width='20px' />
+        </TD>
+      </TR>
+      <TR>
+        <TD align="center" >
+          <Skeleton />
+        </TD>
+        <TD align="center" >
+          <Skeleton />
+        </TD>
+        <TD align="center" >
+          <Skeleton width='100px' />
+        </TD>
+        <TD align="center" >
+          <Skeleton height='20px' width='20px' />
+        </TD>
+      </TR>
+      <TR>
+        <TD align="center" >
+          <Skeleton />
+        </TD>
+        <TD align="center" >
+          <Skeleton />
+        </TD>
+        <TD align="center" >
+          <Skeleton width='100px' />
+        </TD>
+        <TD align="center" >
+          <Skeleton height='20px' width='20px' />
+        </TD>
+      </TR>
+    </>
   )
 }
 export default memo(LoadingUsersTable)
