014c5df5b9ad480a22f37d579d80bc20486266b4
sfwnisme <safymo424@gmail.com>
2025-06-27 03:44:19 +0300

    feat(LoadingTickets): apply the ticket skeleton loading layout depends on the current tickets page layout

--- src/features/tickets/components/loadingTickets/LoadingTickets.tsx
+++ src/features/tickets/components/loadingTickets/LoadingTickets.tsx
@@ -1,16 +1,17 @@
-import Grid from '../../../../components/grid/Grid'
-import TicketSkeleton from '../ticket/TicketSkeleton'
-
+import TicketV2Skeleton from '../ticketV2/TicketV2Skeleton'
+import GridStyle from '../renderTickets/RenderTickets.module.css'
+import ListStyle from '../renderTicketsV2/RenderTicketsV2.module.css'
 export default function LoadingTickets() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   console.log(arr)
+  const list = window.localStorage.getItem('activeLayout') === 'list'
   return (
-    <Grid>
+    <div className={`${list? ListStyle['render-tickets-v2']: GridStyle['render-tickets']}`}>
       {
         arr.map((_) => (
-          <TicketSkeleton key={_} />
+          <TicketV2Skeleton key={_} />
         ))
       }
-    </Grid>
+    </div>
   )
 }
