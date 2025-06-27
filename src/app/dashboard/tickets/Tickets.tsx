a040751992edb72eccf2ce2b5d5d06c41d32ab61
sfwnisme <safymo424@gmail.com>
2025-06-28 01:52:27 +0300

    feat(dashoard): add permission wrapper on create action buttons

--- src/app/dashboard/tickets/Tickets.tsx
+++ src/app/dashboard/tickets/Tickets.tsx
@@ -3,13 +3,16 @@ import { useFloatTicket } from '../../../store/ticket.store.ts';
 import RenderTicketsContent from '../../../features/tickets/components/renderTicketsContent/RenderTicketsContent.tsx';
 import TicketPageInsights from '../../../features/tickets/components/ticketsPageInsights/TicketsPageInsights.tsx';
 import PageHeader from '../../../components/pageHeader/PageHeader.tsx';
+import Can from '../../../components/can/Can.tsx';
 
 export default function Tickets() {
   const isFloatTicketVisible = useFloatTicket((state) => state.isFloatTicketVisible)
   return (
     <div>
       <TicketPageInsights />
-      <PageHeader title='Tickets' button='Create Ticket' href='/dashboard/tickets/create' />
+      <Can permission='canCreate' route='ticket' fallback={<PageHeader title="Tickets" />}>
+        <PageHeader title='Tickets' button='Create Ticket' href='/dashboard/tickets/create' />
+      </Can>
       <RenderTicketsContent />
       {isFloatTicketVisible ? <FloatTicket /> : null}
     </div >
--- src/app/dashboard/users/Users.tsx
+++ src/app/dashboard/users/Users.tsx
@@ -3,11 +3,14 @@ import PageHeader from "../../../components/pageHeader/PageHeader"
 import DeleteUserDialog from "../../../features/users/components/deleteUserDialog/DeleteUserDialog"
 import RenderUsersTable from "../../../features/users/components/tables/RenderUsersTable/RenderUsersTable"
 import { memo } from "react"
+import Can from "../../../components/can/Can"
 
 const Users = () => {
   return (
     <div>
-      <PageHeader title="Users" button="Create user" href="create" />
+      <Can permission="canCreate" route="user" fallback={<PageHeader title="Users" />}>
+        <PageHeader title="Users" button="Create user" href="create" />
+      </Can>
       <UsersTable>
         <RenderUsersTable />
         <DeleteUserDialog />
